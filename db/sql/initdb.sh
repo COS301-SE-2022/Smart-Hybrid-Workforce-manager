c77e58c5da3ea8484c8a593ece4b3e1d785016f6
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-05 12:34:31 +0200

    🗃(db): Added create team function

--- db/sql/initdb.sh
+++ db/sql/initdb.sh
@@ -9,6 +9,7 @@ echo "Using database $DBNAME"
 
 CONFIG=" dbname=$DBNAME user=$USER"
 
+# example
 for file in ${DIR}/example/*.schema.*sql
 do
   psql "$CONFIG" -f "$file"
@@ -19,12 +20,24 @@ do
   psql "$CONFIG" -f "$file"
 done
 
+# user
 for file in ${DIR}/user/*.schema.*sql
 do
   psql "$CONFIG" -f "$file"
 done
 
 for file in ${DIR}/user/*.function.*sql
+do
+  psql "$CONFIG" -f "$file"
+done
+
+# team
+for file in ${DIR}/team/*.schema.*sql
+do
+  psql "$CONFIG" -f "$file"
+done
+
+for file in ${DIR}/team/*.function.*sql
 do
   psql "$CONFIG" -f "$file"
 done
