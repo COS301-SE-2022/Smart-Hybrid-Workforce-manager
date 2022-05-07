e34f9eb301b042c99711774d3d37adc739ef8e2c
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-07 17:37:35 +0200

    🗃(db): Overloaded User Find

--- db/sql/user/user.identifier.function.find.sql
+++ db/sql/user/user.identifier.function.find.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE FUNCTION "user".identifier_find (
 	_identifier varchar(256)
 )
 RETURNS TABLE (
+	id uuid,
     identifier VARCHAR(256),
 	first_name VARCHAR(256),
 	last_name VARCHAR(256),
@@ -12,8 +13,29 @@ RETURNS TABLE (
 $$
 BEGIN
 	RETURN QUERY
-    SELECT i.identifier, i.first_name, i.last_name, i.email, i.picture, i.date_created
+    SELECT i.id, i.identifier, i.first_name, i.last_name, i.email, i.picture, i.date_created
     FROM "user".identifier as i
     WHERE i.identifier = _identifier;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION "user".identifier_find (
+	_id uuid
+)
+RETURNS TABLE (
+	id uuid,
+    identifier VARCHAR(256),
+	first_name VARCHAR(256),
+	last_name VARCHAR(256),
+	email VARCHAR(256),
+	picture VARCHAR(256),
+	date_created TIMESTAMP
+) AS 
+$$
+BEGIN
+	RETURN QUERY
+    SELECT i.id, i.identifier, i.first_name, i.last_name, i.email, i.picture, i.date_created
+    FROM "user".identifier as i
+    WHERE i.id = _id;
 END
 $$ LANGUAGE plpgsql;
