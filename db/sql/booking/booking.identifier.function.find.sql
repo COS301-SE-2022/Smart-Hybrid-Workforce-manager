7eea96cca4834355bf6465b2ae9369e992eb4aaa
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-08 10:18:10 +0200

    🗃(db): Created Booking Remove and Refined other functions

--- db/sql/booking/booking.identifier.function.find.sql
+++ db/sql/booking/booking.identifier.function.find.sql
@@ -1,5 +1,9 @@
 CREATE OR REPLACE FUNCTION booking.identifier_find(
-	_user_id uuid
+	_user_id uuid,
+    _resource_type resource.type DEFAULT NULL,
+    _start TIMESTAMP DEFAULT NULL,
+    _end TIMESTAMP DEFAULT NULL,
+    _booked BOOLEAN DEFAULT NULL
 )
 RETURNS TABLE (
     id uuid,
@@ -16,6 +20,10 @@ BEGIN
     RETURN QUERY
     SELECT i.id, i.user_id, i.resource_type, i.resource_preference_id, i.start, i."end", i.booked, i.date_created
     FROM booking.identifier as i
-    WHERE i.user_id = _user_id;
+    WHERE i.user_id = _user_id
+    AND (_resource_type IS NULL OR i.resource_type = _resource_type)
+    AND (_start IS NULL OR i.start >= _start)
+    AND (_end IS NULL OR i."end" >= _end)
+    AND (_booked IS NULL OR i.booked = _booked);
 END
 $$ LANGUAGE plpgsql;
--- db/sql/booking/booking.identifier.function.store.sql
+++ db/sql/booking/booking.identifier.function.store.sql
@@ -3,13 +3,14 @@ CREATE OR REPLACE FUNCTION booking.identifier_create(
 	_resource_type resource.type,
 	_resource_preference_id uuid,
 	_start TIMESTAMP WITHOUT TIME ZONE,
-	_end TIMESTAMP WITHOUT TIME ZONE
+	_end TIMESTAMP WITHOUT TIME ZONE,
+	_id uuid DEFAULT NULL
 )
-RETURNS BOOLEAN AS 
+RETURNS uuid AS 
 $$
 BEGIN
-    INSERT INTO booking.identifier(user_id, resource_type, resource_preference_id, start, "end")
-    VALUES (_user_id, _resource_type, _resource_preference_id, _start, _end);
-    RETURN TRUE;
+    INSERT INTO booking.identifier(id, user_id, resource_type, resource_preference_id, start, "end")
+    VALUES (COALESCE(_id, uuid_generate_v4()), _user_id, _resource_type, _resource_preference_id, _start, _end);
+    RETURN identifier.id;
 END
 $$ LANGUAGE plpgsql;
