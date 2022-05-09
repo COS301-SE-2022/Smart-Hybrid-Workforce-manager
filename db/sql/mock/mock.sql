8ec2273dc4b7a79aaf5802b051063d7f4d072af0
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-09 12:23:06 +0200

    🪅(db): Added some mock data

--- db/sql/mock/mock.sql
+++ db/sql/mock/mock.sql
@@ -1,7 +1,9 @@
------------------
+----------------------------------
 -- User
 
+-- User 01
 SELECT "user".identifier_store(
+	'11111111-dc08-4a06-9983-8b374586e459'::uuid,
 	'email@example.com', 
 	'Test', 
 	'Tester', 
@@ -9,11 +11,31 @@ SELECT "user".identifier_store(
 	'/picture'
 );
 
------------------
+----------------------------------
+-- Team
+
+
+----------------------------------
 -- Resource
 
-SELECT resource.identifier_create(
-	'aLocation', 
-	null, 
+-- Resource 01
+SELECT resource.identifier_store(
+	'22222222-dc08-4a06-9983-8b374586e459'::uuid,
+	null::uuid,
+	'ALocation', 
+	null::uuid, 
 	'DESK'::resource.type
+);
+
+----------------------------------
+-- Booking
+
+-- Booking 01
+SELECT booking.identifier_store(
+	'33333333-dc08-4a06-9983-8b374586e459'::uuid,
+	'11111111-dc08-4a06-9983-8b374586e459'::uuid, -- User 01
+	'DESK'::resource.type,
+	'22222222-dc08-4a06-9983-8b374586e459'::uuid, -- Resource 01
+	'2022-05-09 09:54:16.865562'::TIMESTAMP,
+	'2022-05-09 13:54:16.865562'::TIMESTAMP
 );
