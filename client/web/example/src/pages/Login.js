b4b84911469268e7d3557f45c13428c0ed8f4aed
Thashil <tash2814@gmail.com>
2022-05-11 15:40:24 +0200

    :lipstick: (ui): Added styling and signup route from login page

--- client/web/example/src/pages/Login.js
+++ client/web/example/src/pages/Login.js
@@ -1,35 +1,35 @@
-import Footer from "../components/Footer"
 import React from 'react'
-import { FaLongArrowAltRight } from 'react-icons/fa'
-import { useNavigate } from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Login()
 {
-  let navigate = useNavigate();
-  const routeSignup = () =>
-  {
-    let path = "/signup";
-    navigate(path);
-  }
-
   return (
     <div>
       <div className='content'>
-        <div className='form-container'>
-          <p><h1>WELCOME BACK</h1>Please enter your details.</p>
-          
-          <Form className='form'>
-            <Form.Group className='form-group' controlId="formBasicEmail">
-              <Form.Label className='form-label'>Email<br></br></Form.Label>
-              <Form.Control className='form-input' type="email" placeholder="Enter email" />
-            </Form.Group>
+        <div className='login-grid'>
+          <div className='form-container'>
+            <p className='form-header'><h1>WELCOME BACK</h1>Please enter your details.</p>
+            
+            <Form className='form'>
+              <Form.Group className='form-group' controlId="formBasicEmail">
+                <Form.Label className='form-label'>Email<br></br></Form.Label>
+                <Form.Control className='form-input' type="email" placeholder="Enter your email" />
+              </Form.Group>
 
-            <Form.Group className='form-group' controlId="formBasicPassword">
-              <Form.Label className='form-label'>Password<br></br></Form.Label>
-              <Form.Control className='form-input' type="password" placeholder="Enter password" />
-            </Form.Group>
-          </Form>
+              <Form.Group className='form-group' controlId="formBasicPassword">
+                <Form.Label className='form-label'>Password<br></br></Form.Label>
+                <Form.Control className='form-input' type="password" placeholder="Enter your password" />
+              </Form.Group>
+
+              <Button className='button-submit' variant='primary' type='submit'>Sign In</Button>
+            </Form>
+            <p className='signup-prompt'>Don't have an account? <a className='signup-link' href='/signup'>Sign up for free!</a></p>
+          </div>
+
+          <div className='image-container'>
+            <img className='login-image' src='https://i.pinimg.com/originals/43/90/d7/4390d72e6a6cb6086c73e570bb6c439d.jpg' alt='office'></img>
+          </div>
         </div>
 
       </div>
