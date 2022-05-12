7555ab0179f2bce2a0379fba93c90ef24e29b613
Thashil <tash2814@gmail.com>
2022-05-12 13:09:57 +0200

    :sparkles: (ui): Added the create bookings functionality using the api

--- client/web/example/src/pages/Signup.js
+++ client/web/example/src/pages/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import Alert from 'react-bootstrap/Alert'
 
 function Signup()
 {
@@ -29,7 +28,8 @@ function Signup()
 
       if(res.status === 200)
       {
-        return <Alert variant='success'>Success!</Alert>;
+        alert("Account Successfully Created!");
+        window.location.assign("./bookings");
       }
     }
     catch(err)
