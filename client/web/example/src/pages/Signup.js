97a3b0d9ab0dd932b7acbb20c03e20ac6740b306
Thashil <tash2814@gmail.com>
2022-05-12 12:43:15 +0200

    :sparkles: (ui): Added the signup functionality using the APUI

--- client/web/example/src/pages/Signup.js
+++ client/web/example/src/pages/Signup.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 function Signup()
 {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let handleSubmit = async (e) =>
+  {
+    e.preventDefault();
+    try
+    {
+      let res = await fetch("http://localhost:8100/api/user/register", 
+      {
+        method: "POST",
+        body: JSON.stringify({
+          id: "",
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          password: password
+        })
+      });
+
+      if(res.status === 200)
+      {
+        return <Alert variant='success'>Success!</Alert>;
+      }
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+  };  
+
   return (
     <div className='page-container'>
       <div className='content-login'>
@@ -11,25 +45,25 @@ function Signup()
           <div className='form-container-signup'>
             <p className='form-header'><h1>CREATE AN ACCOUNT</h1>Please enter your details.</p>
             
-            <Form className='form'>
+            <Form className='form' onSubmit={handleSubmit}>
               <Form.Group className='form-group' controlId="formBasicName">
                 <Form.Label className='form-label'>First Name<br></br></Form.Label>
-                <Form.Control className='form-input' type="text" placeholder="Enter your first name" />
+                <Form.Control className='form-input' type="text" placeholder="Enter your first name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
               </Form.Group>
 
               <Form.Group className='form-group' controlId="formBasicName">
                 <Form.Label className='form-label'>Surname<br></br></Form.Label>
-                <Form.Control className='form-input' type="text" placeholder="Enter your surname" />
+                <Form.Control className='form-input' type="text" placeholder="Enter your surname" value={lastName} onChange={(e) => setLastName(e.target.value)} />
               </Form.Group>
 
               <Form.Group className='form-group' controlId="formBasicEmail">
                 <Form.Label className='form-label'>Email<br></br></Form.Label>
-                <Form.Control className='form-input' type="text" placeholder="Enter your email" />
+                <Form.Control className='form-input' type="text" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
               </Form.Group>
 
               <Form.Group className='form-group' controlId="formBasicPassword">
                 <Form.Label className='form-label'>Password<br></br></Form.Label>
-                <Form.Control className='form-input' type="password" placeholder="Enter your password" />
+                <Form.Control className='form-input' type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
               </Form.Group>
 
               <Button className='button-submit' variant='primary' type='submit'>Create Account</Button>
