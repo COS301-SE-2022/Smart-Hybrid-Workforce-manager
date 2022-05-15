dc03b8e79bd3b510011cb7eb334b992c76d05dcf
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-15 08:23:21 +0200

    🗃(db): Added Resource Room CRUD

---
+++ db/sql/resource/resource.room.function.remove.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION resource.room_remove(
+    _id uuid
+)
+RETURNS TABLE (
+    id uuid,
+	building_id uuid,
+    name VARCHAR(256),
+	location VARCHAR(256),
+	dimension VARCHAR(256)
+) AS 
+$$
+BEGIN
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM resource.room as b
+        WHERE b.id = _id
+        FOR UPDATE
+    ) THEN
+        RAISE EXCEPTION 'invalid_resource'
+            USING HINT = 'Please check the provided resource id parameter';
+    END IF;
+
+    RETURN QUERY
+    DELETE FROM resource.room as a WHERE a.id = _id
+    RETURNING *;
+
+END
+$$ LANGUAGE plpgsql;
