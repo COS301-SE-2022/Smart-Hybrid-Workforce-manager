09e172c636d89bb9b3715767ec5bb4db4ab08ecf
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-15 15:29:48 +0200

    📻(api): Added Role endpoints

--- go/api/data/role.go
+++ go/api/data/role.go
@@ -19,6 +19,16 @@ type Role struct {
 // Roles represent a splice of Role
 type Roles []*Role
 
+// UserRole identifies a UserRole via common attributes
+type UserRole struct {
+	RoleId    *string    `json:"role_id,omitempty"`
+	UserId    *string    `json:"user_id,omitempty"`
+	DateAdded *time.Time `json:"date_added,omitempty"`
+}
+
+// Roles represent a splice of Role
+type UserRoles []*UserRole
+
 // RoleDA provides access to the database for authentication purposes
 type RoleDA struct {
 	access *db.Access
@@ -45,4 +55,128 @@ func mapRole(rows *sql.Rows) (interface{}, error) {
 		return nil, err
 	}
 	return identifier, nil
+}
+
+func mapUserRole(rows *sql.Rows) (interface{}, error) {
+	var identifier UserRole
+	err := rows.Scan(
+		&identifier.RoleId,
+		&identifier.UserId,
+		&identifier.DateAdded,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return identifier, nil
+}
+
+//////////////////////////////////////////////////
+// Functions
+
+// StoreIdentifier stores an identifier
+func (access *RoleDA) StoreIdentifier(identifier *Role) error {
+	_, err := access.access.Query(
+		`SELECT 1 FROM role.identifier_store($1, $2)`, nil,
+		identifier.Id, identifier.RoleName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//FindIdentifier finds an identifier
+func (access *RoleDA) FindIdentifier(identifier *Role) (Roles, error) {
+	results, err := access.access.Query(
+		`SELECT * FROM role.identifier_find($1, $2, $3)`, mapRole,
+		identifier.Id, identifier.RoleName, identifier.DateAdded)
+	if err != nil {
+		return nil, err
+	}
+	tmp := make([]*Role, 0)
+	for r, _ := range results {
+		if value, ok := results[r].(Role); ok {
+			tmp = append(tmp, &value)
+		}
+	}
+	return tmp, nil
+}
+
+//DeleteIdentifier finds an identifier
+func (access *RoleDA) DeleteIdentifier(identifier *Role) (*Role, error) {
+	results, err := access.access.Query(
+		`SELECT * FROM role.identifier_remove($1)`, mapRole,
+		identifier.Id)
+	if err != nil {
+		return nil, err
+	}
+	var tmp Roles
+	tmp = make([]*Role, 0)
+	for r, _ := range results {
+		if value, ok := results[r].(Role); ok {
+			tmp = append(tmp, &value)
+		}
+	}
+	return tmp.FindHead(), nil
+}
+
+//FindHead returns the first Booking
+func (roles Roles) FindHead() *Role {
+	if len(roles) == 0 {
+		return nil
+	}
+	return roles[0]
+}
+
+// StoreUserRole stores an identifier
+func (access *RoleDA) StoreUserRole(identifier *UserRole) error {
+	_, err := access.access.Query(
+		`SELECT 1 FROM role.user_store($1, $2)`, nil,
+		identifier.RoleId, identifier.UserId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//FindUserRole finds an identifier
+func (access *RoleDA) FindUserRole(identifier *UserRole) (UserRoles, error) {
+	results, err := access.access.Query(
+		`SELECT * FROM role.user_find($1, $2, $3)`, mapUserRole,
+		identifier.RoleId, identifier.UserId, identifier.DateAdded)
+	if err != nil {
+		return nil, err
+	}
+	tmp := make([]*UserRole, 0)
+	for r, _ := range results {
+		if value, ok := results[r].(UserRole); ok {
+			tmp = append(tmp, &value)
+		}
+	}
+	return tmp, nil
+}
+
+//DeleteUserRole finds an identifier
+func (access *RoleDA) DeleteUserRole(identifier *UserRole) (*UserRole, error) {
+	results, err := access.access.Query(
+		`SELECT * FROM role.user_remove($1, $2)`, mapUserRole,
+		identifier.RoleId, identifier.UserId)
+	if err != nil {
+		return nil, err
+	}
+	var tmp UserRoles
+	tmp = make([]*UserRole, 0)
+	for r, _ := range results {
+		if value, ok := results[r].(UserRole); ok {
+			tmp = append(tmp, &value)
+		}
+	}
+	return tmp.FindHead(), nil
+}
+
+//FindHead returns the first Booking
+func (roles UserRoles) FindHead() *UserRole {
+	if len(roles) == 0 {
+		return nil
+	}
+	return roles[0]
 }
--- go/api/endpoints/role.go
+++ go/api/endpoints/role.go
@@ -1,13 +1,12 @@
 package endpoints
 
 import (
-	// "api/data"
-	// "api/db"
+	"api/data"
+	"api/db"
 	"api/utils"
-	// "fmt"
-	// "lib/logger"
+	"fmt"
+	"lib/logger"
 	"net/http"
-	// "regexp"
 
 	"github.com/gorilla/mux"
 )
@@ -15,24 +14,246 @@ import (
 //////////////////////////////////////////////////
 // Structures and Variables
 
-
 /////////////////////////////////////////////
 // Endpoints
 
 //ResourceHandlers
 func RoleHandlers(router *mux.Router) error {
-	router.HandleFunc("/create", TempRoleHandler).Methods("POST")
-	router.HandleFunc("/information", TempRoleHandler).Methods("POST")
-	router.HandleFunc("/users", TempRoleHandler).Methods("POST")
-	router.HandleFunc("/remove", TempRoleHandler).Methods("POST")
-	router.HandleFunc("/update", TempRoleHandler).Methods("POST")
+	router.HandleFunc("/create", CreateRoleHandler).Methods("POST")
+	router.HandleFunc("/information", InformationRolesHandler).Methods("POST")
+	router.HandleFunc("/remove", DeleteRoleHandler).Methods("POST")
+
+	router.HandleFunc("/user/create", CreateUserRoleHandler).Methods("POST")
+	router.HandleFunc("/user/information", InformationUserRolesHandler).Methods("POST")
+	router.HandleFunc("/user/remove", DeleteUserRoleHandler).Methods("POST")
 	return nil
 }
 
 /////////////////////////////////////////////
 // Functions
 
-// DeleteBookingHandler rewrites fields for booking where applicable
-func TempRoleHandler(writer http.ResponseWriter, request *http.Request){
+// CreateRoleHandler creates or updates a Role
+func CreateRoleHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.Role
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	// TODO [KP]: Do more checks like if there exists a Role already etc
+
+	err = da.StoreIdentifier(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v created\n", role.Id)
+
 	utils.Ok(writer, request)
+}
+
+// InformationRolesHandler gets Roles
+func InformationRolesHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.Role
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	roles, err := da.FindIdentifier(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v Role information requested\n", role.Id)
+
+	utils.JSONResponse(writer, request, roles)
+}
+
+// DeleteRoleHandler removes a Role
+func DeleteRoleHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.Role
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	roleRemoved, err := da.DeleteIdentifier(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v Role removed\n", role.Id)
+
+	utils.JSONResponse(writer, request, roleRemoved)
+}
+
+// CreateUserRoleHandler creates or updates a Role
+func CreateUserRoleHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.UserRole
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	// TODO [KP]: Do more checks like if there exists a Role already etc
+
+	err = da.StoreUserRole(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v created\n", role.RoleId)
+
+	utils.Ok(writer, request)
+}
+
+// InformationUserRolesHandler gets Roles
+func InformationUserRolesHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.UserRole
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	roles, err := da.FindUserRole(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v Role information requested\n", role.RoleId)
+
+	utils.JSONResponse(writer, request, roles)
+}
+
+// DeleteUserRoleHandler removes a Role
+func DeleteUserRoleHandler(writer http.ResponseWriter, request *http.Request) {
+	var role data.UserRole
+
+	err := utils.UnmarshalJSON(writer, request, &role)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
+
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewRoleDA(access)
+
+	roleRemoved, err := da.DeleteUserRole(&role)
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Printf("%v Role removed\n", role.RoleId)
+
+	utils.JSONResponse(writer, request, roleRemoved)
 }
