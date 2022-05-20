4afc5feaff5e7b506a263928ef900f0a59d62c3b
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-20 13:55:30 +0200

    🔒(security): Added database filtering for resource rooms

--- db/sql/resource/resource.room.function.find.sql
+++ db/sql/resource/resource.room.function.find.sql
@@ -3,7 +3,8 @@ CREATE OR REPLACE FUNCTION resource.room_find(
 	_building_id uuid DEFAULT NULL,
     _name VARCHAR(256) DEFAULT NULL,
     _location VARCHAR(256) DEFAULT NULL,
-    _dimension VARCHAR(256) DEFAULT NULL
+    _dimension VARCHAR(256) DEFAULT NULL,
+    _permissions JSONB DEFAULT NULL
 )
 RETURNS TABLE (
     id uuid,
@@ -14,10 +15,32 @@ RETURNS TABLE (
 ) AS 
 $$
 BEGIN
+    CREATE TEMP TABLE _permissions_table (
+        permission_type permission.type,
+        permission_category permission.category,
+        permission_tenant permission.tenant,
+        permission_tenant_id uuid
+    ) ON COMMIT DROP;
+
+    INSERT INTO _permissions_table (
+    SELECT
+        (jsonb_array_elements(_permissions)->>'permission_type')::permission.type,
+        (jsonb_array_elements(_permissions)->>'permission_category')::permission.category,
+        (jsonb_array_elements(_permissions)->>'permission_tenant')::permission.tenant,
+        (jsonb_array_elements(_permissions)->>'permission_tenant_id')::uuid
+    );
+
     RETURN QUERY
+    WITH permitted_rooms AS (
+        SELECT permission_tenant_id FROM _permissions_table 
+        WHERE permission_type = 'VIEW'::permission.type
+        AND permission_category = 'RESOURCE'::permission.category
+        AND permission_tenant = 'ROOM'::permission.tenant
+    )
     SELECT i.id, i.building_id, i.name, i.location, i.dimension
     FROM resource.room as i
     WHERE (_id IS NULL OR i.id = _id)
+    AND (EXISTS(SELECT 1 FROM permitted_rooms WHERE permission_tenant_id is null) OR i.id = ANY(SELECT * FROM permitted_rooms))
     AND (_building_id IS NULL OR i.building_id = _building_id)
     AND (_name IS NULL OR i.name = _name)
     AND (_location IS NULL OR i.location = _location)
