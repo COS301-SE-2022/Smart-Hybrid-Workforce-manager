0ea071a73d858230ca961f1160baec01df3216c6
Kaylee Posthumus <kayleeposthumus1@gmail.com>
2022-05-20 13:55:09 +0200

    🔒(security): Added database filtering for resource identifiers

--- db/sql/resource/resource.identifier.function.find.sql
+++ db/sql/resource/resource.identifier.function.find.sql
@@ -5,7 +5,8 @@ CREATE OR REPLACE FUNCTION resource.identifier_find(
     _location VARCHAR(256) DEFAULT NULL,
     _role_id uuid DEFAULT NULL,
     _resource_type resource.type DEFAULT NULL,
-    _date_created TIMESTAMP DEFAULT NULL
+    _date_created TIMESTAMP DEFAULT NULL,
+    _permissions JSONB DEFAULT NULL
 )
 RETURNS TABLE (
     id uuid,
@@ -18,10 +19,32 @@ RETURNS TABLE (
 ) AS 
 $$
 BEGIN
+    CREATE TEMP TABLE _permissions_table (
+        permission_type permission.type,
+        permission_category permission.category,
+        permission_tenant permission.tenant,
+        permission_tenant_id uuid
+    ) ON COMMIT DROP;
+
+    INSERT INTO _permissions_table (
+    SELECT
+        (jsonb_array_elements(_permissions)->>'permission_type')::permission.type,
+        (jsonb_array_elements(_permissions)->>'permission_category')::permission.category,
+        (jsonb_array_elements(_permissions)->>'permission_tenant')::permission.tenant,
+        (jsonb_array_elements(_permissions)->>'permission_tenant_id')::uuid
+    );
+
     RETURN QUERY
+    WITH permitted_identifiers AS (
+        SELECT permission_tenant_id FROM _permissions_table 
+        WHERE permission_type = 'VIEW'::permission.type
+        AND permission_category = 'RESOURCE'::permission.category
+        AND permission_tenant = 'IDENTIFIER'::permission.tenant
+    )
     SELECT i.id, i.room_id, i.name, i.location, i.role_id, i.resource_type, i.date_created
     FROM resource.identifier as i
     WHERE (_id IS NULL OR i.id = _id)
+    AND (EXISTS(SELECT 1 FROM permitted_identifiers WHERE permission_tenant_id is null) OR i.id = ANY(SELECT * FROM permitted_identifiers))
     AND (_room_id IS NULL OR i.room_id = _room_id)
     AND (_name IS NULL OR i.name = _name)
     AND (_location IS NULL OR i.location = _location)
