45d6024776348518a03a60c7db42a713153212b7
Jonathan Enslin <u19103345@tuks.co.za>
2022-05-25 20:44:34 +0200

    🚧 (testing) Implemented migration, still needs to be tested, err with db conn

--- go/lib/dbmigrate/dbmigrate.go
+++ go/lib/dbmigrate/dbmigrate.go
@@ -4,7 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
-	"regexp"
+	"os"
+	"path"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -45,7 +46,7 @@ func (gm GoMigrate) Migrate(db *sql.DB) error {
 
 // Manual Migration
 type ManMigrate struct {
-	// A prefix that is appended to each path in MigrateDirs
+	// A prefix that is prepended to each path in MigrateDirs
 	PathPrefix string
 
 	// MigrateDirs is an array of the directories containing the mirgration folders
@@ -53,20 +54,22 @@ type ManMigrate struct {
 	// the order is important if there are dependencies
 	MigrateDirs []string // Required
 
-	// SeqRules contains a set of regex rules, the files in dirs of MigrateDirs will
-	// be evaluated in the order of the SeqRules, see example for further details
-	// If left out, all files will be evaluated and in any order
-	SeqRules []string
+	// PathPatterns contains a set of shell path patterns the files in dirs of MigrateDirs will
+	// be evaluated in the order of the PathPatterns, see example for further details
+	// If left out, all files will be processed and in any order, no file will be processed
+	// more than once.
+	PathPatterns []string
 }
 
 func (mg ManMigrate) Migrate(db *sql.DB) error {
-	paths, regexSeqs, err := mg.setup()
+	fileProcessedMap := make(map[string]bool) // only used to check if key exists
+	patterns, paths, err := mg.setup()
 	if err != nil {
 		return err
 	}
 	for _, dir := range paths {
-		for _, regex := range regexSeqs {
-			fmt.Println(dir)
+		fmt.Println(dir)
+		for _, pathRule := range patterns {
 			files, err := ioutil.ReadDir(dir)
 			if err != nil {
 				fmt.Println(err)
@@ -76,8 +79,18 @@ func (mg ManMigrate) Migrate(db *sql.DB) error {
 				if file.IsDir() {
 					continue // skip directories
 				}
-				if regex.MatchString(file.Name()) {
-					fmt.Println("  ", file.Name())
+				matched, err := path.Match(pathRule, file.Name())
+				if err != nil {
+					return err
+				}
+				_, isPresent := fileProcessedMap[file.Name()]
+				if matched && !isPresent {
+					fmt.Println("  ", file.Name(), "  ", dir+"/"+file.Name())
+					fileProcessedMap[file.Name()] = true
+					err = migrateFile(dir+"/"+file.Name(), db)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
@@ -85,18 +98,24 @@ func (mg ManMigrate) Migrate(db *sql.DB) error {
 	return nil
 }
 
-func (mg ManMigrate) setup() ([]string, []*regexp.Regexp, error) {
-	regexSeqs := make([]*regexp.Regexp, len(mg.SeqRules))
-	var err error
-	for i, regexS := range mg.SeqRules {
-		regexSeqs[i], err = regexp.Compile(regexS)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-	paths := make([]string, len(mg.MigrateDirs))
+func (mg ManMigrate) setup() (patterns []string, paths []string, err error) {
+	// following two lines is to allow for easier change in future if needed
+	patterns = make([]string, len(mg.PathPatterns))
+	copy(patterns, mg.PathPatterns)
+	paths = make([]string, len(mg.MigrateDirs))
 	for i, path := range mg.MigrateDirs {
 		paths[i] = mg.PathPrefix + path
+		// filepath.Join(mg.PathPrefix, path)
 	}
-	return paths, regexSeqs, nil
+	return patterns, paths, nil
+}
+
+// assumes db is already open
+func migrateFile(filepath string, db *sql.DB) error {
+	file, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(string(file))
+	return err
 }
