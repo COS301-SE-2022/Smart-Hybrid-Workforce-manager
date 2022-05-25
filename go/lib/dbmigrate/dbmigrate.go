3149ae9f70a817991cff48ca1bf3e3b0cabda513
Jonathan Enslin <u19103345@tuks.co.za>
2022-05-25 18:31:56 +0200

    🚧 (testing) Added parts of manual migration, using regex

--- go/lib/dbmigrate/dbmigrate.go
+++ go/lib/dbmigrate/dbmigrate.go
@@ -2,6 +2,9 @@ package dbmigrate
 
 import (
 	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"regexp"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -38,4 +41,62 @@ func (gm GoMigrate) Migrate(db *sql.DB) error {
 		return err
 	}
 	return nil
+}
+
+// Manual Migration
+type ManMigrate struct {
+	// A prefix that is appended to each path in MigrateDirs
+	PathPrefix string
+
+	// MigrateDirs is an array of the directories containing the mirgration folders
+	// the directories will be visited in the same sequence as migrateDirs, and thus
+	// the order is important if there are dependencies
+	MigrateDirs []string // Required
+
+	// SeqRules contains a set of regex rules, the files in dirs of MigrateDirs will
+	// be evaluated in the order of the SeqRules, see example for further details
+	// If left out, all files will be evaluated and in any order
+	SeqRules []string
+}
+
+func (mg ManMigrate) Migrate(db *sql.DB) error {
+	paths, regexSeqs, err := mg.setup()
+	if err != nil {
+		return err
+	}
+	for _, dir := range paths {
+		for _, regex := range regexSeqs {
+			fmt.Println(dir)
+			files, err := ioutil.ReadDir(dir)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			for _, file := range files {
+				if file.IsDir() {
+					continue // skip directories
+				}
+				if regex.MatchString(file.Name()) {
+					fmt.Println("  ", file.Name())
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (mg ManMigrate) setup() ([]string, []*regexp.Regexp, error) {
+	regexSeqs := make([]*regexp.Regexp, len(mg.SeqRules))
+	var err error
+	for i, regexS := range mg.SeqRules {
+		regexSeqs[i], err = regexp.Compile(regexS)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	paths := make([]string, len(mg.MigrateDirs))
+	for i, path := range mg.MigrateDirs {
+		paths[i] = mg.PathPrefix + path
+	}
+	return paths, regexSeqs, nil
 }
