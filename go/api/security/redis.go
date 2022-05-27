848ba575acdf4af27e5d001793574d5fe4f6a68c
Estian Nel <84448921+ListlessPanda@users.noreply.github.com>
2022-05-27 05:01:59 +0200

    :sparkles: (api) Redis setup complete
    Redis is setup and can be integrated with api for authentication

---
+++ go/api/security/redis.go
@@ -0,0 +1,46 @@
+package security
+
+import (
+	"lib/logger"
+
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+////////////////////////////////////////////////
+//Structures and Variables
+
+//Redis client --Singleton design pattern-- so use GetRedisClient function to get
+var redisClient *redis.Client
+
+//context of the program for instance of redis running
+var ctx = context.Background()
+
+func GetRedisClient() redis.Client {
+	if redisClient == nil {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "archepassword1234",
+			DB:       0, // use default DB
+		})
+		err := redisClient.Set(ctx, "verify", true, 0).Err()
+		if err != nil {
+			logger.Error.Fatal(err)
+		}
+		val, err := redisClient.Get(ctx, "verify").Result()
+		if err != nil {
+			logger.Error.Fatal(err)
+		}
+		_ = val
+		return *redisClient
+
+	}
+	val, err := redisClient.Get(ctx, "verify").Result()
+	if err != nil {
+		redisClient = nil
+		GetRedisClient()
+	}
+	_ = val
+	return *redisClient
+}
