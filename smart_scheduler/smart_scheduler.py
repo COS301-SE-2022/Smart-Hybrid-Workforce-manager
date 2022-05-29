324f30f677511c9070e968327006bbe8c3b831b0
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-05-29 21:02:57 +0200

    :sparkles: :construction: (scheduler) Added booking finalization functionality to the scheduler

--- smart_scheduler/smart_scheduler.py
+++ smart_scheduler/smart_scheduler.py
@@ -1,15 +1,32 @@
 import docker
 import requests
+import json 
 
-# from flask import Flask
-# app = Flask(__name__) # Flask instance named app
+def init():
+    # response = requests.post("http://arche-api:8080/api/booking/information", data="{}")
+    response = requests.post("http://arche-api:8080/api/booking/information", data='{}')
+    global bookingsList
+    bookingsList = json.loads(response.content)
 
-# client = docker.from_env()
+def printBookings():
+    response = requests.post("http://arche-api:8080/api/booking/information", data='{}')
+    bookingsList = json.loads(response.content)
+    i = 1
+    for booking in bookingsList:
+        print('Booking ' + str(i) + ': ')
+        i+=1
+        for field in booking:
+            print(field + ' : ' + str(booking[field]))
+        print()
 
-# print("Containers")
-# for container in client.containers.list():
-#   print(container.attrs['NetworkSettings']['IPAddress'])
+# only creates the first booking for now
+def finaliseBookings():
+    for booking in bookingsList:
+        if not booking['booked']:
+            response = requests.post("http://arche-api:8080/api/booking/create", data='{"id": "' + str(booking['id']) + '", "user_id": "' + str(booking['user_id']) + '", "resource_type": "' + str(booking['resource_type']) + '", "resource_preference_id": "' + str(booking['resource_preference_id']) + '", "start": "' + str(booking['start']) + '", "end": "' + str(booking['end']) + '", "booked": true}')
+            print(response)
+            break
 
-response = requests.post("http://arche-api:8080/api/user/information", data="{}")
-
-print(response.content)
+init()
+# finaliseBookings()
+# printBookings()
