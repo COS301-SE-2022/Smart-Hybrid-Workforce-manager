8e08a690985f6abd392adb019b37389bdb8d0789
Jonathan Enslin <u19103345@tuks.co.za>
2022-06-09 19:19:10 +0200

    ✨ (scheduler) Endpoint running

--- smart_scheduler/scheduler_endpoint.py
+++ smart_scheduler/scheduler_endpoint.py
@@ -1,11 +1,14 @@
-from pickle import TRUE
-from re import T
 from flask import Flask
+import smart_scheduler
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def hello():
-    return "Hello World!"
-print("what2")
+    print("what on erf") 
+    scheduler = smart_scheduler.SmartScheduler()
+    scheduler.schedule()
+    return scheduler.printBookings()
+    # return "Hello World!"
+print("what2")  # please
 app.run(host="0.0.0.0", port=8081, debug=True)
