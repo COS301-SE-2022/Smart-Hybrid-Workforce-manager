cb477e49c8cbb49b2012f14c468aea5b12a7e3d4
Jonathan Enslin <u19103345@tuks.co.za>
2022-06-09 19:50:02 +0200

    🐛 (scheduler) Fixed endpoint to actually send bookings to api

--- smart_scheduler/scheduler_endpoint.py
+++ smart_scheduler/scheduler_endpoint.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def hello():
-    print("what on erf") 
     scheduler = smart_scheduler.SmartScheduler()
     scheduler.schedule()
+    scheduler.createBookings()
     return scheduler.printBookings()
-    # return "Hello World!"
-print("what2")  # please
 app.run(host="0.0.0.0", port=8081, debug=True)
