96a4ab2d2d28be3b2e7da3d29effa088ce06417d
Thashil <tash2814@gmail.com>
2022-06-09 17:12:11 +0200

    :airplane: (tests): Implemented room information test

--- go/api/endpoints/resource_test.go
+++ go/api/endpoints/resource_test.go
@@ -131,6 +131,101 @@ func TestInformationBuildingsHandler(t *testing.T) {
 	}
 }
 
+func TestInformationRoomsHandler(t *testing.T) {
+	testdb := SetupTest(t)
+	defer dtdb.StopTestDbWithTest(testdb, t, false)
+
+	// ==================
+	// Perform tests ====
+	// ==================
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+
+	type badExpect struct {
+		responseCode    int
+		responseMessage string
+	}
+
+	// Basic Bad tests ================
+	basicBadTests := []struct {
+		name    string
+		request string
+		args    args
+		expect  badExpect
+	}{
+		{
+			name: "Bad JSON, syntax error",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/room/information`, strings.NewReader(`
+				{
+					id: null,
+					buildingId: null,
+					name: "Room A",
+					location: "Room A Location...",
+					dimensions: "5x5"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+		{
+			name: "Bad JSON, array",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/room/information`, strings.NewReader(`
+				[
+					{
+						id: null,
+						buildingId: null,
+						name: "Room A",
+						location: "Room A Location...",
+						dimensions: "5x5"
+					}
+				]`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+	}
+
+	for _, tt := range basicBadTests {
+		t.Run(tt.name, func(t *testing.T) {
+			InformationRoomsHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("VIEW", "RESOURCE", "ROOM")}) // Make request
+			// check response code
+			response := tt.args.w.Result()
+			if response.StatusCode != tt.expect.responseCode {
+				t.Error(tu.Scolourf(tu.RED, "Invalid response code recieved, expected %d, got %d", tt.expect.responseCode, response.StatusCode))
+			}
+			defer response.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Error(tu.Scolourf(tu.RED, "Could not read response body, err: %v", err))
+				t.FailNow()
+			}
+			var _error errorResponse
+			err = json.Unmarshal(bodyBytes, &_error) // decode body
+			if err != nil {
+				t.Errorf(tu.Scolourf(tu.RED, "Invalid JSON, could not decode, err: %v", err))
+				t.FailNow()
+			}
+			if message, ok := _error.Error["message"]; ok {
+				if message != tt.expect.responseMessage {
+					t.Error(tu.Scolourf(tu.RED, "Incorrect message returned, expected '%s', got '%s'", tt.expect.responseMessage, message))
+				}
+			} else {
+				t.Error(tu.Scolourf(tu.RED, "Expected an error message, got none"))
+			}
+		})
+	}
+}
+
 // TODO CREATE TEAM USER
 // TODO REMOVE TEAM USER
 
