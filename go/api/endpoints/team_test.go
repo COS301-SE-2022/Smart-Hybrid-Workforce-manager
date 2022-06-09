74435bcd4ee094479564e55424682a73c2a4f5ff
Thashil <tash2814@gmail.com>
2022-06-09 18:37:03 +0200

    :airplane: (tests): Implemented delete team test

--- go/api/endpoints/team_test.go
+++ go/api/endpoints/team_test.go
@@ -36,9 +36,6 @@ func createTeamAssociation(teamId string, teamAssociationId string) data.TeamAss
 	return userTeam
 }
 
-// TODO CREATE TEAM
-// TODO REMOVE TEAM
-
 func TestInformationTeamHandler(t *testing.T) {
 	testdb := SetupTest(t)
 	defer dtdb.StopTestDbWithTest(testdb, t, false)
@@ -201,6 +198,101 @@ func TestCreateTeamHandler(t *testing.T) {
 	for _, tt := range basicBadTests {
 		t.Run(tt.name, func(t *testing.T) {
 			CreateTeamHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("CREATE", "TEAM", "IDENTIFIER")}) // Make request
+			// check response code
+			response := tt.args.w.Result()
+			if response.StatusCode != tt.expect.responseCode {
+				t.Error(tu.Scolourf(tu.RED, "Invalid response code recieved, expected %d, got %d", tt.expect.responseCode, response.StatusCode))
+			}
+			defer response.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Error(tu.Scolourf(tu.RED, "Could not read response body, err: %v", err))
+				t.FailNow()
+			}
+			var _error errorResponse
+			err = json.Unmarshal(bodyBytes, &_error) // decode body
+			if err != nil {
+				t.Errorf(tu.Scolourf(tu.RED, "Invalid JSON, could not decode, err: %v", err))
+				t.FailNow()
+			}
+			if message, ok := _error.Error["message"]; ok {
+				if message != tt.expect.responseMessage {
+					t.Error(tu.Scolourf(tu.RED, "Incorrect message returned, expected '%s', got '%s'", tt.expect.responseMessage, message))
+				}
+			} else {
+				t.Error(tu.Scolourf(tu.RED, "Expected an error message, got none"))
+			}
+		})
+	}
+}
+
+func TestDeleteTeamHandler(t *testing.T) {
+	testdb := SetupTest(t)
+	defer dtdb.StopTestDbWithTest(testdb, t, false)
+
+	// ==================
+	// Perform tests ====
+	// ==================
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+
+	type badExpect struct {
+		responseCode    int
+		responseMessage string
+	}
+
+	// Basic Bad tests ================
+	basicBadTests := []struct {
+		name    string
+		request string
+		args    args
+		expect  badExpect
+	}{
+		{
+			name: "Bad JSON, syntax error",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/remove`, strings.NewReader(`
+				{
+					id: null,
+					name: "Team A",
+					description: "Team A's description...",
+					capacity: 5
+					picture : "/pic.jpg"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+		{
+			name: "Bad JSON, array",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/remove`, strings.NewReader(`
+				[
+					{
+						id: null,
+						name: "Team A",
+						description: "Team A's description...",
+						capacity: 5
+						picture : "/pic.jpg"
+					}
+				]`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+	}
+
+	for _, tt := range basicBadTests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeleteTeamHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("DELETE", "TEAM", "IDENTIFIER")}) // Make request
 			// check response code
 			response := tt.args.w.Result()
 			if response.StatusCode != tt.expect.responseCode {
