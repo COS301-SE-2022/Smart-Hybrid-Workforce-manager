3db35ce9f2ceb7e9364cf9d921ecb98495aa0ff7
Thashil <tash2814@gmail.com>
2022-06-09 18:43:05 +0200

    :airplane: (tests): Implemented create team association test

--- go/api/endpoints/team_test.go
+++ go/api/endpoints/team_test.go
@@ -592,6 +592,7 @@ func TestDeleteUserTeamHandler(t *testing.T) {
 // TODO CREATE TEAM ASSOCIATION
 // TODO REMOVE TEAM ASSOCIATION
 
+//Team association
 func TestInformationTeamAssociationHandler(t *testing.T) {
 	testdb := SetupTest(t)
 	defer dtdb.StopTestDbWithTest(testdb, t, false)
@@ -620,7 +621,7 @@ func TestInformationTeamAssociationHandler(t *testing.T) {
 			name: "Bad JSON, syntax error",
 			args: args{
 				httptest.NewRecorder(),
-				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/user/information`, strings.NewReader(`
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/association/information`, strings.NewReader(`
 				{
 					team_id : "11111111-1111-4a06-9983-8b374586e459"
 					user_id : "11111111-1111-4a06-9983-8b374586e459"
@@ -635,7 +636,7 @@ func TestInformationTeamAssociationHandler(t *testing.T) {
 			name: "Bad JSON, array",
 			args: args{
 				httptest.NewRecorder(),
-				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/user/information`, strings.NewReader(`
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/association/information`, strings.NewReader(`
 				[
 					{
 						team_id : "11111111-1111-4a06-9983-8b374586e459"
@@ -653,6 +654,95 @@ func TestInformationTeamAssociationHandler(t *testing.T) {
 	for _, tt := range basicBadTests {
 		t.Run(tt.name, func(t *testing.T) {
 			InformationTeamAssociationHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("VIEW", "TEAM", "ASSOCIATION")}) // Make request
+			// check response code
+			response := tt.args.w.Result()
+			if response.StatusCode != tt.expect.responseCode {
+				t.Error(tu.Scolourf(tu.RED, "Invalid response code recieved, expected %d, got %d", tt.expect.responseCode, response.StatusCode))
+			}
+			defer response.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Error(tu.Scolourf(tu.RED, "Could not read response body, err: %v", err))
+				t.FailNow()
+			}
+			var _error errorResponse
+			err = json.Unmarshal(bodyBytes, &_error) // decode body
+			if err != nil {
+				t.Errorf(tu.Scolourf(tu.RED, "Invalid JSON, could not decode, err: %v", err))
+				t.FailNow()
+			}
+			if message, ok := _error.Error["message"]; ok {
+				if message != tt.expect.responseMessage {
+					t.Error(tu.Scolourf(tu.RED, "Incorrect message returned, expected '%s', got '%s'", tt.expect.responseMessage, message))
+				}
+			} else {
+				t.Error(tu.Scolourf(tu.RED, "Expected an error message, got none"))
+			}
+		})
+	}
+}
+
+func TestCreateTeamAssociationHandler(t *testing.T) {
+	testdb := SetupTest(t)
+	defer dtdb.StopTestDbWithTest(testdb, t, false)
+
+	// ==================
+	// Perform tests ====
+	// ==================
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+
+	type badExpect struct {
+		responseCode    int
+		responseMessage string
+	}
+
+	// Basic Bad tests ================
+	basicBadTests := []struct {
+		name    string
+		request string
+		args    args
+		expect  badExpect
+	}{
+		{
+			name: "Bad JSON, syntax error",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/association/create`, strings.NewReader(`
+				{
+					team_id : "11111111-1111-4a06-9983-8b374586e459"
+					user_id : "11111111-1111-4a06-9983-8b374586e459"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+		{
+			name: "Bad JSON, array",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/team/association/create`, strings.NewReader(`
+				[
+					{
+						team_id : "11111111-1111-4a06-9983-8b374586e459"
+						user_id : "11111111-1111-4a06-9983-8b374586e459"
+					}
+				]`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+	}
+
+	for _, tt := range basicBadTests {
+		t.Run(tt.name, func(t *testing.T) {
+			CreateTeamAssociationHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("CREATE", "TEAM", "ASSOCIATION")}) // Make request
 			// check response code
 			response := tt.args.w.Result()
 			if response.StatusCode != tt.expect.responseCode {
