81b71f47a50c14e66f916e0295af1b3cb6db5c0c
Jonathan Enslin <u19103345@tuks.co.za>
2022-07-18 14:51:14 +0200

    🚧 (api/scheduler) Added callOnDay function with partially implemented logic

--- go/api/scheduler/caller.go
+++ go/api/scheduler/caller.go
@@ -1,6 +1,9 @@
 package scheduler
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 const LOG_PATH string = "////"
 
@@ -29,8 +32,10 @@ func mayCall(scheduledDay string, lastEntry *LogEntry, now time.Time) bool {
 	return false
 }
 
-func run() error {
-	scheduledDay := "Friday" // TODO: @JonathanEnslin Make env var
+// CheckAndCall will access the logs, and then call the scheduler if the info log
+// entry permits it
+func checkAndCall(scheduledDay string) error {
+	// scheduledDay := "Friday" // TODO: @JonathanEnslin Make env var
 	now := time.Now()
 	lastEntry, err := ReadLastEntry(LOG_PATH)
 	if err != nil {
@@ -48,7 +53,19 @@ func call() {
 	// TODO: @JonathanEnslin Implement
 }
 
-func callPeriodically() {
-	// TODO @JonathanEnslin
-	// Call the scheduler on set intervals
+// callOnDay will call checkAndCall() on each recurring certain day of the week
+func callOnDay(ctx context.Context, scheduledDay string) {
+	// Initial call, for whenn the function initially gets called
+	checkAndCall(scheduledDay)
+	// periodic calls
+	stopLoop := false
+	for !stopLoop {
+		timer := time.NewTimer(time.Until(time.Now())) // TODO: @JonathanEnslin Change stubbed until time
+		select {
+		case <-timer.C:
+			checkAndCall(scheduledDay)
+		case <-ctx.Done():
+			stopLoop = true
+		}
+	}
 }
