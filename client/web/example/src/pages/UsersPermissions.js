6e3edcdd148e80f338eebf3c7111f9edf183061d
KayleePosthumus <kayleeposthumus1@gmail.com>
2022-07-25 09:52:51 +0200

    🪄(ui): Added meaningful user permissions

--- client/web/example/src/pages/UsersPermissions.js
+++ client/web/example/src/pages/UsersPermissions.js
@@ -5,259 +5,244 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import '../App.css'
 
-function TeamPermissions()
-{
-  const [teamName, setTeamName] = useState(window.sessionStorage.getItem("TeamName"));
-  const [teampermissions, SetTeamPermissions] = useState([]);
+function UserPermissions() {
+  const [userName, setUserName] = useState(window.sessionStorage.getItem("UserName"));
+  const [userPermissions, SetUserPermissions] = useState([]);
 
-  const [createTeamIdentifier, SetCreateTeamIdentifier] = useState("") // allows a user to update the team
-  const [createTeamIdentifierId, SetCreateTeamIdentifierId] = useState("")
-  const [viewTeamIdentifier, SetViewTeamIdentifier] = useState("") // allows a user to view the team
-  const [viewTeamIdentifierId, SetViewTeamIdentifierId] = useState("")
-  const [deleteTeamIdentifier, SetDeleteTeamIdentifier] = useState("") // allows a user to delete the team
-  const [deleteTeamIdentifierId, SetDeleteTeamIdentifierId] = useState("")
+  // Resource
+  const [createResourceIdentifier, SetCreateResourceIdentifier] = useState("") // allows a user to update the Resource
+  const [createResourceIdentifierId, SetCreateResourceIdentifierId] = useState("")
+  const [viewResourceIdentifier, SetViewResourceIdentifier] = useState("") // allows a user to view the Resource
+  const [viewResourceIdentifierId, SetViewResourceIdentifierId] = useState("")
+  const [deleteResourceIdentifier, SetDeleteResourceIdentifier] = useState("") // allows a user to delete the Resource
+  const [deleteResourceIdentifierId, SetDeleteResourceIdentifierId] = useState("")
 
-
-  const [createTeamUser, SetCreateTeamUser] = useState("") // allows users to add a user of the team
-  const [createTeamUserId, SetCreateTeamUserId] = useState("")
-  const [viewTeamUser, SetViewTeamUser] = useState("") // allows users to view users of the team
-  const [viewTeamUserId, SetViewTeamUserId] = useState("")
-  const [deleteTeamUser, SetDeleteTeamUser] = useState("") // allows users to remove users of the team
-  const [deleteTeamUserId, SetDeleteTeamUserId] = useState("")
+  // Room
+  const [createRoomIdentifier, SetCreateRoomIdentifier] = useState("") // allows a user to update the Room
+  const [createRoomIdentifierId, SetCreateRoomIdentifierId] = useState("")
+  const [viewRoomIdentifier, SetViewRoomIdentifier] = useState("") // allows a user to view the Room
+  const [viewRoomIdentifierId, SetViewRoomIdentifierId] = useState("")
+  const [deleteRoomIdentifier, SetDeleteRoomIdentifier] = useState("") // allows a user to delete the Room
+  const [deleteRoomIdentifierId, SetDeleteRoomIdentifierId] = useState("")
 
   let handleSubmit = async (e) => {
     e.preventDefault();
-    if (createTeamIdentifier && createTeamIdentifierId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "CREATE", "TEAM", "IDENTIFIER");
+
+    // Resource
+    if (createResourceIdentifier && createResourceIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "CREATE", "RESOURCE", "IDENTIFIER");
     }
-    if (!createTeamIdentifier && createTeamIdentifierId != null)
-    {
-      RemovePermission(createTeamIdentifierId);
+    if (!createResourceIdentifier && createResourceIdentifierId != null) {
+      RemovePermission(createResourceIdentifierId);
     }
 
-    if (viewTeamIdentifier && viewTeamIdentifierId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "VIEW", "TEAM", "IDENTIFIER");
+    if (viewResourceIdentifier && viewResourceIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "VIEW", "RESOURCE", "IDENTIFIER");
     }
-    if (!viewTeamIdentifier && viewTeamIdentifierId != null)
-    {
-      RemovePermission(viewTeamIdentifierId);
+    if (!viewResourceIdentifier && viewResourceIdentifierId != null) {
+      RemovePermission(viewResourceIdentifierId);
     }
 
-    if (deleteTeamIdentifier && deleteTeamIdentifierId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "DELETE", "TEAM", "IDENTIFIER");
+    if (deleteResourceIdentifier && deleteResourceIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "DELETE", "RESOURCE", "IDENTIFIER");
     }
-    if (!deleteTeamIdentifier && deleteTeamIdentifierId != null)
-    {
-      RemovePermission(deleteTeamIdentifierId);
+    if (!deleteResourceIdentifier && deleteResourceIdentifierId != null) {
+      RemovePermission(deleteResourceIdentifierId);
     }
 
-    if (createTeamUser && createTeamUserId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "CREATE", "TEAM", "USER");
+    // Room
+    if (createRoomIdentifier && createRoomIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "CREATE", "ROOM", "IDENTIFIER");
     }
-    if (!createTeamUser && createTeamUserId != null)
-    {
-      RemovePermission(createTeamUserId);
-    }
-
-    if (viewTeamUser && viewTeamUserId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "VIEW", "TEAM", "USER");
-    }
-    if (!viewTeamUser && viewTeamUserId != null)
-    {
-      RemovePermission(viewTeamUserId);
+    if (!createRoomIdentifier && createRoomIdentifierId != null) {
+      RemovePermission(createRoomIdentifierId);
     }
 
-    if (deleteTeamUser && deleteTeamUserId === "")
-    {
-      AddPermission(window.sessionStorage.getItem("TeamID"), "DELETE", "TEAM", "USER");
+    if (viewRoomIdentifier && viewRoomIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "VIEW", "ROOM", "IDENTIFIER");
     }
-    if (!deleteTeamUser && deleteTeamUserId != null)
-    {
-      RemovePermission(deleteTeamUserId);
+    if (!viewRoomIdentifier && viewRoomIdentifierId != null) {
+      RemovePermission(viewRoomIdentifierId);
     }
-    
+
+    if (deleteRoomIdentifier && deleteRoomIdentifierId === "") {
+      AddPermission(window.sessionStorage.getItem("UserID"), "USER", "DELETE", "ROOM", "IDENTIFIER");
+    }
+    if (!deleteRoomIdentifier && deleteRoomIdentifierId != null) {
+      RemovePermission(deleteRoomIdentifierId);
+    }
+
     alert("Permissions successfully updated.")
   };
 
-  async function AddPermission(id, type, category, tenant) 
-  {
-    try
-    {
-      let res = await fetch("http://localhost:8100/api/permission/create", 
-      {
-        method: "POST",
-        body: JSON.stringify({
-          permission_id: id,
-          permission_id_type: "TEAM",
-          permission_type: type,
-          permission_category: category,
-          permission_tenant: tenant
-        })
-      });
+  async function AddPermission(id, idType, type, category, tenant) {
+    try {
+      let res = await fetch("http://localhost:8100/api/permission/create",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            permission_id: id,
+            permission_id_type: idType,
+            permission_type: type,
+            permission_category: category,
+            permission_tenant: tenant
+          })
+        });
 
-      if (res.status === 200) { 
+      if (res.status === 200) {
       }
     }
-    catch(err)
-    {
+    catch (err) {
       console.log(err);
     }
   };
 
-  async function RemovePermission(id) 
-  {
-    try
-    {
-      let res = await fetch("http://localhost:8100/api/permission/remove", 
-      {
-        method: "POST",
-        body: JSON.stringify({
-          id: id,
-        })
-      });
+  async function RemovePermission(id) {
+    try {
+      let res = await fetch("http://localhost:8100/api/permission/remove",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            id: id,
+          })
+        });
 
-      if (res.status === 200) { 
+      if (res.status === 200) {
       }
     }
-    catch(err)
-    {
+    catch (err) {
       console.log(err);
     }
   };
 
   //POST request
-  const FetchTeamPermissions = () =>
-  {
-    fetch("http://localhost:8100/api/permission/information", 
-        {
-          method: "POST",
-          body: JSON.stringify({
-            permission_id: window.sessionStorage.getItem("TeamID"),
-            permission_id_type: "TEAM",
-          })
-        }).then((res) => res.json()).then(data => 
-        {
-          SetTeamPermissions(data);
-          data.forEach(setPermissionStates);
-        });
+  const FetchUserPermissions = () => {
+    fetch("http://localhost:8100/api/permission/information",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          permission_id: window.sessionStorage.getItem("UserID"),
+          permission_id_type: "USER",
+        })
+      }).then((res) => res.json()).then(data => {
+        SetUserPermissions(data);
+        data.forEach(setPermissionStates);
+      });
   }
 
-  function setPermissionStates(permission)
-  {
-    if (permission.permission_type === "CREATE")
-    {
-      if (permission.permission_tenant === "IDENTIFIER")
-      {
-        SetCreateTeamIdentifier(true);
-        SetCreateTeamIdentifierId(permission.id);
-      }        
-      if (permission.permission_tenant === "USER")
-      {
-        SetCreateTeamUser(true);
-        SetCreateTeamUserId(permission.id);
+  function setPermissionStates(permission) {
+    // Resource
+    if (permission.permission_category === 'RESOURCE') {
+      if (permission.permission_type === "CREATE") {
+        if (permission.permission_tenant === "IDENTIFIER") {
+          SetCreateResourceIdentifier(true);
+          SetCreateResourceIdentifierId(permission.id);
+        }
+      }
+      if (permission.permission_type === "VIEW") {
+        if (permission.permission_tenant === "IDENTIFIER") {
+          SetViewResourceIdentifier(true);
+          SetViewResourceIdentifierId(permission.id);
+        }
+      }
+      if (permission.permission_type === "DELETE") {
+        if (ppermission.permission_tenant === "IDENTIFIER") {
+          SetDeleteResourceIdentifier(true);
+          SetDeleteResourceIdentifierId(permission.id);
+        }
       }
     }
-    if (permission.permission_type === "VIEW")
-    {
-      if (permission.permission_tenant === "IDENTIFIER")
-      {
-        SetViewTeamIdentifier(true);
-        SetViewTeamIdentifierId(permission.id);
-      }        
-      if (permission.permission_tenant === "USER")
-      {
-        SetViewTeamUser(true);
-        SetViewTeamUserId(permission.id);
-      }
-    }
-    if (permission.permission_type === "DELETE")
-    {
-      if (permission.permission_tenant === "IDENTIFIER")
-      {
-        SetDeleteTeamIdentifier(true);
-        SetDeleteTeamIdentifierId(permission.id);
-      }        
-      if (permission.permission_tenant === "USER")
-      {
-        SetDeleteTeamUser(true);
-        SetDeleteTeamUserId(permission.id);
-      }
-    }
-  }
     
-  useEffect(() =>
-  {
-    FetchTeamPermissions();
+    // Room
+    if (permission.permission_category === 'ROOM') {
+      if (permission.permission_type === "CREATE") {
+        if (permission.permission_tenant === "IDENTIFIER") {
+          SetCreateRoomIdentifier(true);
+          SetCreateRoomIdentifierId(permission.id);
+        }
+      }
+      if (permission.permission_type === "VIEW") {
+        if (permission.permission_tenant === "IDENTIFIER") {
+          SetViewRoomIdentifier(true);
+          SetViewRoomIdentifierId(permission.id);
+        }
+      }
+      if (permission.permission_type === "DELETE") {
+        if (ppermission.permission_tenant === "IDENTIFIER") {
+          SetDeleteRoomIdentifier(true);
+          SetDeleteRoomIdentifierId(permission.id);
+        }
+      }
+    }
+  }
 
-    setTeamName(window.sessionStorage.getItem("TeamName"));
+  useEffect(() => {
+    FetchUserPermissions();
+
+    setUserName(window.sessionStorage.getItem("UserName"));
   }, [])
-    
+
   return (
     <div className='page-container'>
       <div className='content'>
         <Navbar />
         <div className='form-container-team'>
-          <p className='form-header'><h1>Team {teamName}</h1> Configure Permissions</p>
+          <p className='form-header'><h1>User {userName}</h1> Configure Permissions</p>
 
           <Form className='form' onSubmit={handleSubmit}>
-            <h2 className='permission-category'>Team</h2>
+            <h2 className='permission-category'>Resources</h2>
             <Form.Group className='form-group' controlId="formBasicName">
               <label className="container">
-                Allow users of the team to update team information  
-                <input className='checkbox' type="checkbox" checked={createTeamIdentifier} onChange={(e) => SetCreateTeamIdentifier(e.target.checked)}/>
+                Allow user to create resources
+                <input className='checkbox' type="checkbox" checked={createResourceIdentifier} onChange={(e) => SetCreateResourceIdentifier(e.target.checked)} />
                 <span className="checkmark"></span>
               </label>
             </Form.Group>
 
             <Form.Group className='form-group' controlId="formBasicName">
               <label className="container">
-                Allow users of the team to view team information and enrollment  
-                <input className='checkbox' type="checkbox" checked={viewTeamIdentifier} onChange={(e) => SetViewTeamIdentifier(e.target.checked)}/>
+                Allow user to view resources
+                <input className='checkbox' type="checkbox" checked={viewResourceIdentifier} onChange={(e) => SetViewResourceIdentifier(e.target.checked)} />
                 <span className="checkmark"></span>
               </label>
             </Form.Group>
 
             <Form.Group className='form-group' controlId="formBasicName">
               <label className="container">
-                Allow users of the team to delete the team  
-                <input className='checkbox' type="checkbox" checked={deleteTeamIdentifier} onChange={(e) => SetDeleteTeamIdentifier(e.target.checked)}/>
+                Allow user to delete resources
+                <input className='checkbox' type="checkbox" checked={deleteResourceIdentifier} onChange={(e) => SetDeleteResourceIdentifier(e.target.checked)} />
                 <span className="checkmark"></span>
               </label>
             </Form.Group>
 
-            <h2 className='permission-category'>Users</h2>
+            <h2 className='permission-category'>Rooms</h2>
             <Form.Group className='form-group' controlId="formBasicName">
               <label className="container">
-                Allow users to add users to the team
-                <input className='checkbox' type="checkbox" checked={createTeamUser} onChange={(e) => SetCreateTeamUser(e.target.checked)}/>
-                <span className="checkmark"></span>
-              </label>
-            </Form.Group>
-
-            <Form.Group className='form-group' controlId="formBasicName">
-              <label className="container">
-                Allow users to view users of the team  
-                <input className='checkbox' type="checkbox" checked={viewTeamUser} onChange={(e) => SetViewTeamUser(e.target.checked)}/>
+                Allow user to create rooms
+                <input className='checkbox' type="checkbox" checked={createRoomIdentifier} onChange={(e) => SetCreateRoomIdentifier(e.target.checked)} />
                 <span className="checkmark"></span>
               </label>
             </Form.Group>
 
             <Form.Group className='form-group' controlId="formBasicName">
               <label className="container">
-                Allow users to remove users from the team
-                <input className='checkbox' type="checkbox" checked={deleteTeamUser} onChange={(e) => SetDeleteTeamUser(e.target.checked)}/>
+                Allow user to view rooms
+                <input className='checkbox' type="checkbox" checked={viewRoomIdentifier} onChange={(e) => SetViewRoomIdentifier(e.target.checked)} />
                 <span className="checkmark"></span>
               </label>
             </Form.Group>
-            
-            <Button className='button-submit' variant='primary' type='submit'>Update Team Permissions</Button>      
+
+            <Form.Group className='form-group' controlId="formBasicName">
+              <label className="container">
+                Allow user to delete rooms
+                <input className='checkbox' type="checkbox" checked={deleteRoomIdentifier} onChange={(e) => SetDeleteRoomIdentifier(e.target.checked)} />
+                <span className="checkmark"></span>
+              </label>
+            </Form.Group>
+
+            <Button className='button-submit' variant='primary' type='submit'>Update User Permissions</Button>
           </Form>
-          
+
         </div>
       </div>
       <Footer />
@@ -265,4 +250,4 @@ function TeamPermissions()
   )
 }
 
-export default TeamPermissions
+export default UserPermissions
