961451c596e796ec7b510f347f1932b5496c6876
Thashil <tash2814@gmail.com>
2022-07-27 21:23:49 +0200

    :wastebasket: (ui): Cleaned up unused code

--- client/web/example/src/store/protectedPage.js
+++ client/web/example/src/store/protectedPage.js
@@ -1,12 +1,12 @@
-import React, { Component, useContext } from "react";
+/*import { Component, useContext } from "react";
 import { Route, Redirect, useLocation } from "react-router-dom";
 import userProvidor from "./userContext";
 import { userContext } from "./userContext";
-import { useUser } from "./userContext.js";
+import { useUser } from "./userContext.js";*/
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedPage({children}){
-    const {token, setToken} = useUser();
+    //const {token, setToken} = useUser();
 
     const isLoggedIn = token;
     return(
