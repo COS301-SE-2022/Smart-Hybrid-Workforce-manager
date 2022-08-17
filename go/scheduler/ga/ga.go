17542126b01fa1afd859d05304134409e63656f4
Jonathan Enslin <u19103345@tuks.co.za>
2022-08-18 00:47:14 +0200

    🐛 (go/scheduler) Fixed bugs in popgen and ga

--- go/scheduler/ga/ga.go
+++ go/scheduler/ga/ga.go
@@ -1,9 +1,11 @@
 package ga
 
 import (
+	"fmt"
 	"lib/utils"
 	"math/rand"
 	"scheduler/data"
+	"strings"
 )
 
 // Individuals is a collection of solutions to the domain problem
@@ -35,16 +37,20 @@ type Individual struct {
 	Fitness float64
 }
 
-// TODO: @JonathanEnslin update the clone function to be a deep copy
 // Clone clones an individual
 func (individual *Individual) Clone() *Individual {
-	newIndividual := &Individual{Gene: individual.Gene}
+	newGene := make([][]string, len(individual.Gene))
+	for i := 0; i < len(individual.Gene); i++ {
+		newGene[i] = make([]string, len(individual.Gene[i]))
+		copy(newGene[i], individual.Gene[i])
+	}
+	newIndividual := &Individual{Gene: newGene, Fitness: individual.Fitness}
 	return newIndividual
 }
 
 // ClonePopulation clones an entire population
 func (population Individuals) ClonePopulation() []*Individual {
-	cloned := make([]*Individual, 0)
+	cloned := make([]*Individual, len(population))
 	for _, individual := range population {
 		cloned = append(cloned, individual.Clone())
 	}
@@ -62,15 +68,15 @@ func GA(domain Domain, crossover Crossover, fitness Fitness, mutate Mutate, sele
 	rand.Seed(int64(domain.Config.Seed))
 
 	// Create initial pop and calculate fitnesses
-	population := populationGenerator(&domain, 1)
+	population := populationGenerator(&domain, 5)
 	fitness(&domain, population) // TODO Change to useful individuals
 
 	// Run ga
 	stoppingCondition := true
 	for i := 0; i < domain.Config.Generations && stoppingCondition; i++ {
-		crossOverAmount := (domain.Config.Size * int(domain.Config.CrossOverRate))
-		mutateAmount := (domain.Config.Size * int(domain.Config.MutationRate))
-		carryAmount := domain.Config.Size - crossOverAmount - mutateAmount // TODO: Find out Anna if is guicci
+		crossOverAmount := (domain.Config.PopulationSize * int(domain.Config.CrossOverRate))
+		mutateAmount := (domain.Config.PopulationSize * int(domain.Config.MutationRate))
+		carryAmount := domain.Config.PopulationSize - crossOverAmount - mutateAmount // TODO: Find out Anna if is guicci
 
 		// evolve
 		individualsOffspring := crossover(&domain, population, selection, crossOverAmount)
@@ -94,3 +100,69 @@ func GA(domain Domain, crossover Crossover, fitness Fitness, mutate Mutate, sele
 // emp1     emp2    emp3
 // Mon		Mon
 // 0-1		0-1
+func (individual Individual) String() string {
+	// Returns table representation of an individual
+	// 	userIds := make(map[string]int)
+	// 	for dayi := range individual.Gene {
+	// 		for _, userId := range individual.Gene[dayi] {
+	// 			if _, ok := userIds[userId]; !ok && !(userId == "") {
+	// 				userIds[userId] = len(userIds)
+	// 			}
+	// 		}
+	// 	}
+	maxSlotsize := -1
+	for _, day := range individual.Gene {
+		if len(day) > maxSlotsize {
+			maxSlotsize = len(day)
+		}
+	}
+	table := make([][]string, len(individual.Gene))
+	for i := range table {
+		table[i] = make([]string, maxSlotsize+3) // +1 for day thing + 1 for ending border thingy
+	}
+
+	for i := 0; i < len(individual.Gene); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			table[i][j] = strings.Repeat(" ", 39)
+		}
+	}
+
+	for i := range individual.Gene {
+		table[i][0] = fmt.Sprintf("%38s|", fmt.Sprintf("Day-%d", i))
+		table[i][1] = strings.Repeat("=", 39)
+	}
+
+	for i := 0; i < len(individual.Gene); i++ {
+		counter := 0
+		for j := 0; j < len(individual.Gene[i]); j++ {
+			counter = j
+			table[i][j+2] = individual.Gene[i][j]
+			table[i][j+2] = fmt.Sprintf("%38s|", individual.Gene[i][j])
+
+		}
+		table[i][counter+3] = strings.Repeat("-", 38) + "|"
+	}
+
+	// More borders
+	for i := 1; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			if table[i-1][j] == strings.Repeat(" ", 39) && table[i][j] != strings.Repeat(" ", 39) {
+				table[i-1][j] = fmt.Sprintf("%38s|", "")
+			}
+		}
+	}
+
+	tableStr := ""
+	// for key, val := range userIds {
+	// 	tableStr += fmt.Sprintf("%s => %d\n", key, val)
+	// }
+	// tableStr += "\n"
+
+	for j := 0; j < len(table[0]); j++ {
+		for i := 0; i < len(table); i++ {
+			tableStr += table[i][j]
+		}
+		tableStr += "\n"
+	}
+	return tableStr
+}
--- go/scheduler/ga/popgen.go
+++ go/scheduler/ga/popgen.go
@@ -2,6 +2,8 @@ package ga
 
 import (
 	cu "lib/collectionutils"
+	"lib/logger"
+	"lib/testutils"
 	"lib/utils"
 )
 
@@ -28,10 +30,27 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 	for i := 0; i < numDaysInWeek; i++ { // TODO: @JonathanEnslin find out if we want to include all 7 days of week
 		slotSizes = append(slotSizes, len(domain.SchedulerData.Resources)) // Initially assume all slots would be open
 	}
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "1-%v", slotSizes))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
 	for _, booking := range *schedulerData.CurrentBookings {
+		logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "1.5-%v", booking))
 		day := int(booking.GetWeekday())
 		slotSizes[day]-- // Minus one slot for each existing booking on a day
 	}
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "2-%v", slotSizes))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
 	// Make collection of available slots (used to select rand open slots later)
 	// 2D where each row is a day
 	openSlots := make([][]int, 0)
@@ -42,11 +61,29 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 		}
 	}
 
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "3-%v", openSlots))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
 	// Keep track of how many days users are already coming into the office
 	timesAlreadyComingIn := make(map[string]int, 0) // (map[user id]times coming in already)
 	for _, booking := range *schedulerData.CurrentBookings {
 		timesAlreadyComingIn[*booking.UserId]++ // Add one to indicate they are coming in
 	}
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// b, err := json.MarshalIndent(timesAlreadyComingIn, "", "  ")
+	// if err != nil {
+	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "4-%v", err))
+	// }
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "4-----\n%s", string(b)))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
 
 	// Add users as many times as they need to come into office
 	usersToAdd := []string{}
@@ -59,15 +96,38 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 		}
 	}
 
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "5.1-%v", usersToAdd))
+	// b, err = json.MarshalIndent(daysAvailable, "", "  ")
+	// if err != nil {
+	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "5.2-%v", err))
+	// }
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "5.2------\n%s", string(b)))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
 	// Merge this into other loop for cleaner less verbose code!!!
 	// Remove days that users already come in from their available days
 	for _, booking := range *schedulerData.CurrentBookings {
 		bUserId := *booking.UserId
 		if _, ok := daysAvailable[bUserId]; ok {
 			// remove
-			daysAvailable[bUserId] = cu.RemoveElementNoOrder(daysAvailable[bUserId], int(booking.GetWeekday()))
+			daysAvailable[bUserId] = cu.RemoveElementNoOrder(daysAvailable[bUserId], int(booking.GetWeekday())) // inefficient
 		}
 	}
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// b, err = json.MarshalIndent(daysAvailable, "", "  ")
+	// if err != nil {
+	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "6-%v", err))
+	// }
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "6------\n%s", string(b)))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
 
 	openDays := cu.SequentialSequence(0, 7) // Used to keep track of what days still have space open
 	for i := range openSlots {
@@ -76,13 +136,37 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 		}
 	}
 
-	openDaysCopy := []int{}
-	population := make([]*Individual, domain.Config.Size)
-	for i := 0; i < domain.Config.Size; i++ {
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "7-%v", openDays))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "12-%v", openDays))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// END DEBUG ================
+	openDaysCopy := make([]int, len(openDays))
+	population := make([]*Individual, domain.Config.PopulationSize)
+	daysAvailableCopy := make(map[string][]int)
+	for i := 0; i < popSize; i++ {
+		// make copy of days available map
+		for key, val := range daysAvailable {
+			daysAvailableCopy[key] = make([]int, len(val))
+			copy(daysAvailableCopy[key], val)
+		}
 		copy(openDaysCopy, openDays)
+		// DEBUG ================
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "11-%v", openDaysCopy))
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// END DEBUG ================
 		// Used to maintain a copy of openSlots
 		openSlotsCopy := make([][]int, len(openSlots))
 		for k := range openSlotsCopy {
+			openSlotsCopy[k] = make([]int, len(openSlots[k]))
 			copy(openSlotsCopy[k], openSlots[k])
 		}
 		individual := Individual{Gene: make([][]string, numDaysInWeek)}
@@ -93,8 +177,25 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 
 		// Randomly assign employees to slots
 		for _, userId := range usersToAdd {
-			availableDaysIntersection := cu.IntSliceIntersection(daysAvailable[userId], openDaysCopy)
+			// DEBUG ================
+			// logger.Error.Println(testutils.Scolourf(testutils.BLUE, ">>>>>>>>>>>>>>%v<<<<<<<<<<<<<", loop))
+			// time.Sleep(25 * time.Millisecond)
+			// END DEBUG ================
+			availableDaysIntersection := cu.IntSliceIntersection(daysAvailableCopy[userId], openDaysCopy)
+			// DEBUG ================
+			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "9-%v", len(availableDaysIntersection)))
+			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+			// time.Sleep(25 * time.Millisecond)
+			// END DEBUG ================
 			randDay := availableDaysIntersection[utils.RandInt(0, len(availableDaysIntersection))]
+			// DEBUG ================
+			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "Rand day - 10.1-%v", randDay))
+			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "10.2-%v", len(openSlotsCopy[randDay])))
+			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+			// time.Sleep(25 * time.Millisecond)
+			// END DEBUG ================
 			// Get random slot index
 			randSloti := utils.RandInt(0, len(openSlotsCopy[randDay]))
 			randSlot := openSlotsCopy[randDay][randSloti]
@@ -102,13 +203,39 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 			individual.Gene[randDay][randSlot] = userId // Assign the slot
 
 			// Update structures indicating available slots/days
-			daysAvailable[userId] = cu.RemoveElementNoOrder(daysAvailable[userId], randDay)
+			daysAvailableCopy[userId] = cu.RemoveElementNoOrder(daysAvailableCopy[userId], randDay)
 			openSlotsCopy[randDay] = cu.RemElemenAtI(openSlotsCopy[randDay], randSloti)
 			if len(openSlotsCopy[randDay]) == 0 { // If no more spots open
 				openDaysCopy = cu.RemoveElementNoOrder(openDaysCopy, randDay)
 			}
 		}
 		population[i] = &individual
+
+		// DEBUG ================
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "13------\n%s", individual))
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// time.Sleep(100 * time.Millisecond)
+		// END DEBUG ================
+
+		// DEBUG ================
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// b, err = json.MarshalIndent(individual, "", "  ")
+		// if err != nil {
+		// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "8-%v", err))
+		// }
+		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "8------\n%s", string(b)))
+		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+		// time.Sleep(100 * time.Millisecond)
+		// END DEBUG ================
 	}
+
+	// DEBUG ================
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// logger.Error.Println(testutils.Scolour(testutils.GREEN, "Done"))
+	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
+	// time.Sleep(100 * time.Millisecond)
+	// END DEBUG ================
+
 	return population
 }
