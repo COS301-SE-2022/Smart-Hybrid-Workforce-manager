bf308bd4822e6e2fa31265fbea5fe184ff189ecf
Jonathan Enslin <u19103345@tuks.co.za>
2022-08-17 12:32:15 +0200

    ✨ (lib) Added RemoveElementNoOrder slice util

--- go/scheduler/ga/mutate.go
+++ go/scheduler/ga/mutate.go
@@ -3,3 +3,9 @@ package ga
 func StubMutate(domain *Domain, individuals Individuals) Individuals {
 	return individuals.ClonePopulation()
 }
+
+func mutate(domain *Domain, individual Individuals) {
+
+}
+
+func mutateIndividual()
