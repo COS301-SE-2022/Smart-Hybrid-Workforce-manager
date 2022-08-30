e36236f44ee9d92db56e1b09f5d8b6f07bfa59c4
Thashil Naidoo <tash2814@gmail.com>
2022-08-30 17:15:24 +0200

    :lipstick: (ui): Added icons to the navbar

--- client/web/example/src/components/Navbar/Navbar.js
+++ client/web/example/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { useNavigate } from "react-router-dom"
+import { FaCalendar, FaTicketAlt, FaMap, FaChartPie } from 'react-icons/fa'
 
 const Navbar = (props, ref) =>
 {
@@ -13,7 +14,31 @@ const Navbar = (props, ref) =>
 
     return (
         <div ref={ref} className='navbar-container'>
-            W
+            <div className='logo-container'>
+                S.H.W.M
+            </div>
+            <div className='navlink-container'>
+                <div className='navlink'>
+                    <FaCalendar />
+                    &nbsp;
+                    Calendar
+                </div>
+                <div className='navlink'>
+                    <FaTicketAlt />
+                    &nbsp;
+                    Bookings
+                </div>
+                <div className='navlink'>
+                    <FaMap />
+                    &nbsp;
+                    Office Layout
+                </div>
+                <div className='navlink'>
+                    <FaChartPie />
+                    &nbsp;
+                    Statistics
+                </div>
+            </div>
         </div>
     )
 }
