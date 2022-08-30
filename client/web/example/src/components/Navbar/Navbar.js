2cef2aa0cc44d91ab562e809e2fa9cb0bc89fc4d
Thashil Naidoo <tash2814@gmail.com>
2022-08-30 17:31:50 +0200

    :lipstick: (ui): Added working links to the navbar elements

--- client/web/example/src/components/Navbar/Navbar.js
+++ client/web/example/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { useNavigate } from "react-router-dom"
@@ -6,11 +6,78 @@ import { FaCalendar, FaTicketAlt, FaMap, FaChartPie } from 'react-icons/fa'
 
 const Navbar = (props, ref) =>
 {
-    const [startDate, setStartDate] = useState("");
-    const [startTime, setStartTime] = useState("");
-    const [endTime, setEndTime] = useState("");
+    const homeRef = useRef(null);
+    const bookingsRef = useRef(null);
+    const mapRef = useRef(null);
+    const statisticsRef = useRef(null);
+
+    const [currLocation, setCurrLocation] = useState("");
 
     const navigate = useNavigate();
+
+    const NavigateHome = () =>
+    {
+        setCurrLocation("/home")
+        navigate("/");
+    }
+
+    const NavigateBookings = () =>
+    {
+        setCurrLocation("/bookings")
+        navigate("/");
+    }
+
+    const NavigateMap = () =>
+    {
+        setCurrLocation("/map")
+        navigate("/");
+    }
+
+    const NavigateStatistics = () =>
+    {
+        setCurrLocation("/statistics")
+        navigate("/");
+    }
+
+    useEffect(() =>
+    {
+        if(homeRef.current)
+        {
+            if(currLocation === "/home")
+            {
+                homeRef.current.style.color = "#09a2fb";
+            }
+        }
+
+        if(bookingsRef.current)
+        {
+            if(currLocation === "/bookings")
+            {
+                bookingsRef.current.style.color = "#09a2fb";
+            }
+        }
+
+        if(mapRef.current)
+        {
+            if(currLocation === "/map")
+            {
+                mapRef.current.style.color = "#09a2fb";
+            }
+        }
+
+        if(statisticsRef.current)
+        {
+            if(currLocation === "/statistics")
+            {
+                statisticsRef.current.style.color = "#09a2fb";
+            }
+        }
+    },[currLocation])
+
+    useEffect(() =>
+    {
+        setCurrLocation(window.location.pathname);
+    },[])
 
     return (
         <div ref={ref} className='navbar-container'>
@@ -18,22 +85,22 @@ const Navbar = (props, ref) =>
                 S.H.W.M
             </div>
             <div className='navlink-container'>
-                <div className='navlink'>
+                <div ref={homeRef} className='navlink' onClick={NavigateHome}>
                     <FaCalendar />
                     &nbsp;
                     Calendar
                 </div>
-                <div className='navlink'>
+                <div ref={bookingsRef} className='navlink' onClick={NavigateBookings}>
                     <FaTicketAlt />
                     &nbsp;
                     Bookings
                 </div>
-                <div className='navlink'>
+                <div ref={mapRef} className='navlink' onClick={NavigateMap}>
                     <FaMap />
                     &nbsp;
-                    Office Layout
+                    Office Map
                 </div>
-                <div className='navlink'>
+                <div ref={statisticsRef} className='navlink' onClick={NavigateStatistics}>
                     <FaChartPie />
                     &nbsp;
                     Statistics
