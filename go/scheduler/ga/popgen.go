c6044faa055f4953377e48afd7d6819f7b156519
Jonathan Enslin <u19103345@tuks.co.za>
2022-09-05 01:26:48 +0200

    ♻️ (go/scheduler) Refactor DayVResourceMutate

--- go/scheduler/ga/popgen.go
+++ go/scheduler/ga/popgen.go
@@ -20,147 +20,15 @@ func StubPopulationGenerator(domain *Domain, popSize int) Individuals {
 }
 
 func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
+	// Using representation 1 (DayVResource)
 	const numDaysInWeek int = 5 // Assume 5 days in a week for now
 
-	// Using representation 1 (DayVResource)
-	schedulerData := domain.SchedulerData
+	// Get slot info
+	slotSizes, openSlots, openDays := getSlotInfo(domain, numDaysInWeek)
 
-	// logger.Error.Println(testutils.Scolourf(testutils.RED, "amount %v", len(*domain.SchedulerData.CurrentBookings)))
-	// for _, booking := range *domain.SchedulerData.CurrentBookings {
-	// 	logger.Error.Println(testutils.Scolourf(testutils.RED, "%v  %v  %v", *booking.Id, *booking.UserId, *booking.Start))
-	// 	fmt.Println(booking.Id, booking.UserId, booking.Start)
-	// }
+	// Get user related info
+	daysAvailable, usersToAdd := getUserAvailabilityInfo(domain, numDaysInWeek)
 
-	// Get slot sizes/day
-	slotSizes := []int{}
-	for i := 0; i < numDaysInWeek; i++ { // TODO: @JonathanEnslin find out if we want to include all 7 days of week
-		slotSizes = append(slotSizes, len(domain.SchedulerData.Resources)) // Initially assume all slots would be open
-	}
-
-	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	logger.Error.Println(testutils.Scolourf(testutils.GREEN, "slot sizes-%v", slotSizes))
-	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "1-%v", slotSizes))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	for _, booking := range *schedulerData.CurrentBookings {
-		logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "1.5-%v", booking))
-		day := int(booking.GetWeekday())
-		slotSizes[day]-- // Minus one slot for each existing booking on a day
-	}
-
-	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	logger.Error.Println(testutils.Scolourf(testutils.BLUE, "slot sizes-%v", slotSizes))
-	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "2-%v", slotSizes))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// Make collection of available slots (used to select rand open slots later)
-	// 2D where each row is a day
-	openSlots := make([][]int, 0)
-	for dayOfWeek := 0; dayOfWeek < numDaysInWeek; dayOfWeek++ {
-		openSlots = append(openSlots, []int{})
-		for i := 0; i < slotSizes[dayOfWeek]; i++ {
-			openSlots[dayOfWeek] = append(openSlots[dayOfWeek], i)
-		}
-	}
-
-	openDays := cu.SequentialSequence(0, numDaysInWeek) // Used to keep track of what days still have space open // TODO: @Jonathan :SCHEDULER: change to use numDaysInWeek not literal 7
-	for i := range openSlots {
-		if len(openSlots[i]) == 0 {
-			openDays = cu.RemoveElementNoOrder(openDays, i)
-		}
-	}
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "7-%v", openDays))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "3-%v", openSlots))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// Keep track of how many days users are already coming into the office
-	timesAlreadyComingIn := make(map[string]int, 0) // (map[user id]times coming in already)
-	for _, booking := range *schedulerData.CurrentBookings {
-		timesAlreadyComingIn[*booking.UserId]++ // Add one to indicate they are coming in
-	}
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// b, err := json.MarshalIndent(timesAlreadyComingIn, "", "  ")
-	// if err != nil {
-	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "4-%v", err))
-	// }
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "4-----\n%s", string(b)))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// Add users as many times as they need to come into office
-	usersToAdd := []string{}
-	daysAvailable := make(map[string][]int, 0) // Used to indicate what days they are not already coming in (map[user id][days available])
-	for _, user := range domain.SchedulerData.Users {
-		daysAvailable[*user.Id] = cu.SequentialSequence(0, numDaysInWeek)
-		// Add them times they have to come in - days they already come in
-		for i := 0; i < *user.OfficeDays-timesAlreadyComingIn[*user.Id]; i++ { // TODO: @JonathanEnslin find out what do if no office days?
-			usersToAdd = append(usersToAdd, *user.Id)
-		}
-	}
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "5.1-%v", usersToAdd))
-	// b, err = json.MarshalIndent(daysAvailable, "", "  ")
-	// if err != nil {
-	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "5.2-%v", err))
-	// }
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "5.2------\n%s", string(b)))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// Merge this into other loop for cleaner less verbose code!!!
-	// Remove days that users already come in from their available days
-	for _, booking := range *schedulerData.CurrentBookings {
-		bUserId := *booking.UserId
-		if _, ok := daysAvailable[bUserId]; ok {
-			// remove
-			daysAvailable[bUserId] = cu.RemoveElementNoOrder(daysAvailable[bUserId], int(booking.GetWeekday())) // inefficient
-		}
-	}
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// b, err = json.MarshalIndent(daysAvailable, "", "  ")
-	// if err != nil {
-	// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "6-%v", err))
-	// }
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "6------\n%s", string(b)))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
-	// END DEBUG ================
-
-	// DEBUG ================
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "12-%v", openDays))
-	// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// END DEBUG ================
 	openDaysCopy := make([]int, len(openDays))
 	population := make([]*Individual, popSize)
 	daysAvailableCopy := make(map[string][]int)
@@ -171,52 +39,31 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 			copy(daysAvailableCopy[key], val)
 		}
 		copy(openDaysCopy, openDays)
-		// DEBUG ================
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "11-%v", openDaysCopy))
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// END DEBUG ================
+
 		// Used to maintain a copy of openSlots
 		openSlotsCopy := make([][]int, len(openSlots))
 		for k := range openSlotsCopy {
 			openSlotsCopy[k] = make([]int, len(openSlots[k]))
 			copy(openSlotsCopy[k], openSlots[k])
 		}
+
 		individual := Individual{Gene: make([][]string, numDaysInWeek)}
 		// Initialise individual to empty indiv
 		for j := range individual.Gene { // DEBUG how long is GENE?
-			logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-			logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "slot sizes-%v", slotSizes))
-			logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
 			individual.Gene[j] = make([]string, slotSizes[j])
 		}
 
 		// Randomly assign employees to slots
 		for _, userId := range usersToAdd {
-			// DEBUG ================
-			// logger.Error.Println(testutils.Scolourf(testutils.BLUE, ">>>>>>>>>>>>>>%v<<<<<<<<<<<<<", loop))
-			// time.Sleep(25 * time.Millisecond)
-			// END DEBUG ================
 			availableDaysIntersection := cu.IntSliceIntersection(daysAvailableCopy[userId], openDaysCopy)
 			// If user can not come in, either because they already come in each day they can, or
 			// if there are no resources left
 			if len(availableDaysIntersection) == 0 {
 				break
 			}
-			// DEBUG ================
-			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "9-%v", len(availableDaysIntersection)))
-			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-			// time.Sleep(25 * time.Millisecond)
-			// END DEBUG ================
+
 			randDay := availableDaysIntersection[utils.RandInt(0, len(availableDaysIntersection))]
-			// DEBUG ================
-			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "Rand day - 10.1-%v", randDay))
-			// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "10.2-%v", len(openSlotsCopy[randDay])))
-			// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-			// time.Sleep(25 * time.Millisecond)
-			// END DEBUG ================
+
 			// Get random slot index
 			randSloti := utils.RandInt(0, len(openSlotsCopy[randDay]))
 			randSlot := openSlotsCopy[randDay][randSloti]
@@ -231,32 +78,86 @@ func DayVResourcePopulationGenerator(domain *Domain, popSize int) Individuals {
 			}
 		}
 		population[i] = &individual
-
-		// DEBUG ================
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "13------\n%s", individual))
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// time.Sleep(100 * time.Millisecond)
-		// END DEBUG ================
-
-		// DEBUG ================
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// b, err = json.MarshalIndent(individual, "", "  ")
-		// if err != nil {
-		// 	logger.Error.Println(testutils.Scolourf(testutils.YELLOW, "8-%v", err))
-		// }
-		// logger.Error.Println(testutils.Scolourf(testutils.PURPLE, "8------\n%s", string(b)))
-		// logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-		// time.Sleep(100 * time.Millisecond)
-		// END DEBUG ================
 	}
 
 	// DEBUG ================
 	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
 	logger.Error.Println(testutils.Scolour(testutils.GREEN, "Done"))
 	logger.Error.Println(testutils.Scolour(testutils.PURPLE, "============================="))
-	// time.Sleep(100 * time.Millisecond)
 	// END DEBUG ================
 
 	return population
+}
+
+// getSlotInfo used the data in the scheduler and determines the amount of slots available on each day, and the
+// days with open slots, it returns the slotSizes per day, a 2D array where the index corresponds to the day,
+// and each row contains a sequence of numbers, corresponding to an open slot, and openDays, an array indicating
+// which days still have space open
+func getSlotInfo(domain *Domain, numDaysInWeek int) (slotSizes []int, openSlots [][]int, openDays []int) {
+	schedulerData := domain.SchedulerData
+	// Get slot sizes/day
+	slotSizes = []int{}
+	for i := 0; i < numDaysInWeek; i++ { // TODO: @JonathanEnslin find out if we want to include all 7 days of week
+		slotSizes = append(slotSizes, len(domain.SchedulerData.Resources)) // Initially assume all slots would be open
+	}
+
+	for _, booking := range *schedulerData.CurrentBookings {
+		day := int(booking.GetWeekday())
+		slotSizes[day]-- // Minus one slot for each existing booking on a day
+	}
+
+	// Make collection of available slots (used to select rand open slots later)
+	// 2D where each row is a day
+	openSlots = make([][]int, 0)
+	for dayOfWeek := 0; dayOfWeek < numDaysInWeek; dayOfWeek++ {
+		openSlots = append(openSlots, []int{})
+		for i := 0; i < slotSizes[dayOfWeek]; i++ {
+			openSlots[dayOfWeek] = append(openSlots[dayOfWeek], i)
+		}
+	}
+
+	openDays = cu.SequentialSequence(0, numDaysInWeek) // Used to keep track of what days still have space open
+	for i := range openSlots {
+		if len(openSlots[i]) == 0 {
+			openDays = cu.RemoveElementNoOrder(openDays, i)
+		}
+	}
+
+	return slotSizes, openSlots, openDays
+}
+
+// getUserAvailibilityInfo uses the schedulerData to determine which days users are available to come in, as well as the days
+// that the users are available to come into the office, it returns daysAvailable, which is a mop where the keys are user ids and
+// the values are int arrays, where each element in the day is an int that corresponds to a day of the week that the user is
+// available on, it also returns usersToAdd, an array of user ids, where the user id is present in the array as many times
+// as the user needs to come into the office in the week
+func getUserAvailabilityInfo(domain *Domain, numDaysInWeek int) (daysAvailable map[string][]int, usersToAdd []string) {
+	schedulerData := domain.SchedulerData
+	// Keep track of how many days users are already coming into the office
+	timesAlreadyComingIn := make(map[string]int, 0) // (map[user id]times coming in already)
+	for _, booking := range *schedulerData.CurrentBookings {
+		timesAlreadyComingIn[*booking.UserId]++ // Add one to indicate they are coming in
+	}
+
+	// Add users as many times as they need to come into office
+	usersToAdd = []string{}
+	daysAvailable = make(map[string][]int, 0) // Used to indicate what days they are not already coming in (map[user id][days available])
+	for _, user := range domain.SchedulerData.Users {
+		daysAvailable[*user.Id] = cu.SequentialSequence(0, numDaysInWeek)
+		// Add them times they have to come in - days they already come in
+		for i := 0; i < *user.OfficeDays-timesAlreadyComingIn[*user.Id]; i++ { // TODO: @JonathanEnslin find out what do if no office days?
+			usersToAdd = append(usersToAdd, *user.Id)
+		}
+	}
+
+	// Remove days that users already come in from their available days
+	for _, booking := range *schedulerData.CurrentBookings {
+		bUserId := *booking.UserId
+		if _, ok := daysAvailable[bUserId]; ok {
+			// remove
+			daysAvailable[bUserId] = cu.RemoveElementNoOrder(daysAvailable[bUserId], int(booking.GetWeekday())) // inefficient
+		}
+	}
+
+	return daysAvailable, usersToAdd
 }
