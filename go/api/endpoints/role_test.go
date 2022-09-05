4de13160f9008ecea6076d82e5ed84d49c477083
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-05 18:38:15 +0200

    :white_check_mark: (testing/api) Added basic bad tests for DeleteRoleHandler

--- go/api/endpoints/role_test.go
+++ go/api/endpoints/role_test.go
@@ -255,7 +255,7 @@ func TestInformationRolesHandler(t *testing.T) {
 			name: "Bad JSON, syntax error",
 			args: args{
 				httptest.NewRecorder(),
-				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(`
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/information`, strings.NewReader(`
 				{
 					role_name" : "Test Role"
 				}`)),
@@ -269,7 +269,7 @@ func TestInformationRolesHandler(t *testing.T) {
 			name: "Bad JSON, array",
 			args: args{
 				httptest.NewRecorder(),
-				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(`
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/information`, strings.NewReader(`
 				[
 					{
     					"role_lead_id" : "11111111-1111-4a06-9983-8b374586e459"
@@ -282,10 +282,10 @@ func TestInformationRolesHandler(t *testing.T) {
 			},
 		},
 		{
-			name: "Bad JSON, empty",
+			name: "Bad request, empty body",
 			args: args{
 				httptest.NewRecorder(),
-				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(``)),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/information`, strings.NewReader(``)),
 			},
 			expect: badExpect{
 				responseCode:    http.StatusBadRequest,
@@ -296,7 +296,7 @@ func TestInformationRolesHandler(t *testing.T) {
 
 	for _, tt := range basicBadTests {
 		t.Run(tt.name, func(t *testing.T) {
-			CreateRoleHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("", "", "")}) // Make request
+			InformationRolesHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("", "", "")}) // Make request
 			// check response code
 			response := tt.args.w.Result()
 			if response.StatusCode != tt.expect.responseCode {
@@ -326,7 +326,101 @@ func TestInformationRolesHandler(t *testing.T) {
 }
 
 func TestDeleteRoleHandler(t *testing.T) {
+	testdb := ts.SetupTest(t)
+	defer dtdb.StopTestDbWithTest(testdb, t, false)
 
+	// ==================
+	// Perform tests ====
+	// ==================
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+
+	type badExpect struct {
+		responseCode    int
+		responseMessage string
+	}
+	
+	// Basic Bad tests ================
+	basicBadTests := []struct {
+		name    string
+		request string
+		args    args
+		expect  badExpect
+	}{
+		{
+			name: "Bad JSON, syntax error",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/remove`, strings.NewReader(`
+				{
+					role_name" : "Test Role"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+		{
+			name: "Bad JSON, array",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/remove`, strings.NewReader(`
+				[
+					{
+    					"role_lead_id" : "11111111-1111-4a06-9983-8b374586e459"
+					}
+				]`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+		{
+			name: "Bad request, empty body",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/remove`, strings.NewReader(``)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusBadRequest,
+				responseMessage: "invalid_request",
+			},
+		},
+	}
+
+	for _, tt := range basicBadTests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeleteRoleHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("", "", "")}) // Make request
+			// check response code
+			response := tt.args.w.Result()
+			if response.StatusCode != tt.expect.responseCode {
+				t.Error(tu.Scolourf(tu.RED, "Invalid response code recieved, expected %d, got %d", tt.expect.responseCode, response.StatusCode))
+			}
+			defer response.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Error(tu.Scolourf(tu.RED, "Could not read response body, err: %v", err))
+				t.FailNow()
+			}
+			var _error errorResponse
+			err = json.Unmarshal(bodyBytes, &_error) // decode body
+			if err != nil {
+				t.Errorf(tu.Scolourf(tu.RED, "Invalid JSON, could not decode, err: %v", err))
+				t.FailNow()
+			}
+			if message, ok := _error.Error["message"]; ok {
+				if message != tt.expect.responseMessage {
+					t.Error(tu.Scolourf(tu.RED, "Incorrect message returned, expected '%s', got '%s'", tt.expect.responseMessage, message))
+				}
+			} else {
+				t.Error(tu.Scolourf(tu.RED, "Expected an error message, got none"))
+			}
+		})
+	}
 }
 
 func TestCreateUserRoleHandler(t *testing.T) {
