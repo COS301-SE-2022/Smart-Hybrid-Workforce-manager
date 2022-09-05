0e16721c5a9f09d77d5e2c8a1c22d5b4959f8ff3
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-05 17:26:20 +0200

    :white_check_mark: (testing)(api) Added further bad tests for CreateRoleHandler

--- go/api/endpoints/role_test.go
+++ go/api/endpoints/role_test.go
@@ -142,4 +142,87 @@ func TestCreateRoleHandler(t *testing.T) {
 			defer response.Body.Close()
 		})
 	}
+
+	// Further Bad Tests ================
+	badTests := []struct {
+		name    string
+		request string
+		args    args
+		expect  badExpect
+	}{
+		{
+			name: "Bad Data, null role name",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(`
+				{
+					"role_name" : null
+
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusInternalServerError,
+				responseMessage: "",
+			},
+		},
+		{
+			name: "Bad Data, invalid role lead id",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(`
+				{
+					"role_name" : "Test Role 2",
+					"role_lead_id" : "inval"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusInternalServerError,
+				responseMessage: "",
+			},
+		},
+		{
+			name: "Bad Data, duplicate role name",
+			args: args{
+				httptest.NewRecorder(),
+				httptest.NewRequest(http.MethodPost, `http://localhost:8100/api/role/create`, strings.NewReader(`
+				{
+					"role_name" : "Test_Role"
+				}`)),
+			},
+			expect: badExpect{
+				responseCode:    http.StatusInternalServerError,
+				responseMessage: "",
+			},
+		},
+	}
+
+	for _, tt := range badTests {
+		t.Run(tt.name, func(t *testing.T) {
+			CreateRoleHandler(tt.args.w, tt.args.r, &data.Permissions{data.CreateGenericPermission("", "", "")}) // Make request
+			// check response code
+			response := tt.args.w.Result()
+			if response.StatusCode != tt.expect.responseCode {
+				t.Error(tu.Scolourf(tu.RED, "Invalid response code recieved, expected %d, got %d", tt.expect.responseCode, response.StatusCode))
+			}
+			defer response.Body.Close()
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Error(tu.Scolourf(tu.RED, "Could not read response body, err: %v", err))
+				t.FailNow()
+			}
+			var _error errorResponse
+			err = json.Unmarshal(bodyBytes, &_error) // decode body
+			if err != nil {
+				t.Errorf(tu.Scolourf(tu.RED, "Invalid JSON, could not decode, err: %v", err))
+				t.FailNow()
+			}
+			// if message, ok := _error.Error["message"]; ok {
+			// 	if message != tt.expect.responseMessage {
+			// 		t.Error(tu.Scolourf(tu.RED, "Incorrect message returned, expected '%s', got '%s'", tt.expect.responseMessage, message))
+			// 	}
+			// } else {
+			// 	t.Error(tu.Scolourf(tu.RED, "Expected an error message, got none"))
+			// }
+		})
+	}
 }
