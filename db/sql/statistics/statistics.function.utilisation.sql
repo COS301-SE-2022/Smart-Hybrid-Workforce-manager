bfadc8069131a780a75d1e44d655205109232fe6
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-15 19:25:36 +0200

    :card_file_box: Added utilization statistics function

---
+++ db/sql/statistics/statistics.function.utilisation.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION statistics.utilisation 
+(
+	_start_date DATE DEFAULT current_date - interval '7 days',
+	_end_date 	DATE DEFAULT current_date
+)
+RETURNS TABLE (
+	resource_id uuid,
+	utilisation_percent	float
+) AS
+$$
+	DECLARE
+		num_days	INTEGER;
+	BEGIN
+		RETURN QUERY
+			SELECT r.id AS resource_id, ((SELECT COUNT(DISTINCT b.start::date) 
+										 FROM booking.identifier b 
+										 WHERE b.resource_preference_id = r.id  AND (b.start::date BETWEEN _start_date AND _end_date)
+										 GROUP BY b.start
+										)/(_end_date::date - _start_date::date + 1)::float)*100::float AS utilisation_percent
+			FROM resource.identifier r;
+	
+		RETURN;
+	END
+$$ LANGUAGE plpgsql;
+
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
