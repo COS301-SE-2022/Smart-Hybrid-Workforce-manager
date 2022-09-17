2a9aa80498191424279af25f77494803d948c4d2
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-17 13:53:59 +0200

    :white_check_mark: (testing) Added tests to ProfileBar for logging out

--- client/web/example/src/components/Navbar/__test__/ProfileBar.test.js
+++ client/web/example/src/components/Navbar/__test__/ProfileBar.test.js
@@ -21,7 +21,8 @@ const MockProfileBar = () => {
 };
 
 beforeEach(() => {
-  jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+  jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate);
+  jest.spyOn(window.localStorage.__proto__, 'removeItem');
 });
 
 afterEach(cleanup);
@@ -33,5 +34,23 @@ describe('When clicking on Profile picture', () => {
     expect(screen.getByTestId('profilepic-container')).toBeInTheDocument();
     fireEvent.click(screen.getByTestId('profilepic-container'));
     expect(navigate).toHaveBeenCalledWith('/profile');
+  });
+});
+
+describe('When clicking on Logout', () => {
+  it('should navigate to /login', () => {
+    render(<MockProfileBar/> );
+
+    expect(screen.getByText(/Logout/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/Logout/i));
+    expect(navigate).toHaveBeenCalledWith('/login');
+  });
+
+  it('should clear local storage', () => {
+    render(<MockProfileBar/> );
+
+    expect(screen.getByText(/Logout/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/Logout/i));
+    expect(localStorage.removeItem).toHaveBeenCalledWith('auth_data');
   });
 });
