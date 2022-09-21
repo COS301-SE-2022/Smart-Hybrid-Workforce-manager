572060b2170c3215c410ec0f96029b1925c99da5
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-21 13:48:29 +0200

    :sparkles: (api) Added GetAllStatistics endpoint

--- go/api/endpoints/statistics.go
+++ go/api/endpoints/statistics.go
@@ -1,6 +1,11 @@
 package endpoints
 
 import (
+	"api/data"
+	"api/db"
+	"fmt"
+	"lib/logger"
+	"lib/utils"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -8,10 +13,42 @@ import (
 
 //StatisticsHandlers registers the user
 func StatisticsHandlers(router *mux.Router) error {
-	router.HandleFunc("/resource_utilisation", ResourceUtilisation).Methods("POST")
+	// router.HandleFunc("/resource_utilisation", ResourceUtilisation).Methods("POST")
+	router.HandleFunc("/all", AllHandler).Methods("POST")
 	return nil
 }
 
-func ResourceUtilisation(writer http.ResponseWriter, request *http.Request) {
+func AllHandler(writer http.ResponseWriter, request *http.Request) {
+	var statistics data.OverallStatics
+	err := utils.UnmarshalJSON(writer, request, &statistics)
+	if err != nil {
+		fmt.Println(err)
+		utils.BadRequest(writer, request, "invalid_request")
+		return
+	}
 
+	// Create a database connection
+	access, err := db.Open()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+	defer access.Close()
+
+	da := data.NewStatisticsDA(access)
+	all_statistics, err := da.GetAllStatistics()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	// Commit transaction
+	err = access.Commit()
+	if err != nil {
+		utils.InternalServerError(writer, request, err)
+		return
+	}
+
+	logger.Access.Print("All statistics information requested\n")
+	utils.JSONResponse(writer, request, all_statistics)
 }
