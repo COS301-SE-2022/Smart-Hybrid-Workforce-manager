5b360195ff6c1ef8ec8da967eaead567e150ed6c
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-21 13:47:52 +0200

    :sparkles: Added backend for statistics endpoints

--- go/api/data/statistics.go
+++ go/api/data/statistics.go
@@ -22,7 +22,20 @@ func mapOverallStatics(rows *sql.Rows) (interface{}, error) {
 }
 
 type ResourceUtilisation struct {
-	Percentage *string
+	Id         *string `json:"id"`
+	Percentage *string `json:"percentage"`
+}
+
+func mapUtilisation(rows *sql.Rows) (interface{}, error) {
+	var identifier ResourceUtilisation
+	err := rows.Scan(
+		&identifier.Id,
+		&identifier.Percentage,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return identifier, nil
 }
 
 // StatisticsDA
@@ -45,6 +58,18 @@ func (access *StatisticsDA) Commit() error {
 //GetAllStatistics
 // Go look at the FindIdentifier function in data/team.go
 func (access *StatisticsDA) GetAllStatistics() (*OverallStatics, error) {
-
-	return nil, nil
+	results, err := access.access.Query(
+		`SELECT * FROM statistics.utilisation()`, mapUtilisation)
+	if err != nil {
+		return nil, err
+	}
+	tmp := make([]ResourceUtilisation, 0)
+	for r, _ := range results {
+		if value, ok := results[r].(ResourceUtilisation); ok {
+			tmp = append(tmp, value)
+		}
+	}
+	tmpRes := OverallStatics{ResourceUtilisation: tmp}
+	// tmpRes.ResourceUtilisation := tmp
+	return &tmpRes, nil
 }
