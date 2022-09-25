65ca9720e2f5b19b2ea8f06f41ceb0fbe4e9eda1
InThePipeFiveByFive <92717675+InThePipeFiveByFive@users.noreply.github.com>
2022-09-25 17:25:16 +0200

    🗃 (db) Added yearly utilization function

--- db/sql/statistics/statistics.function.average_utilisation.sql
+++ db/sql/statistics/statistics.function.average_utilisation.sql
@@ -11,7 +11,7 @@ $$
 		RETURN
 			(SELECT AVG(COALESCE(((SELECT COUNT(DISTINCT b.start::date) 
                                    FROM booking.identifier b 
-								   WHERE b.resource_preference_id = r.id  AND (b.start::date BETWEEN _start_date AND _end_date)
+								   WHERE b.resource_id = r.id  AND (b.start::date BETWEEN _start_date AND _end_date) AND b.booked = TRUE 
 								   GROUP BY b.start
 								   )/(_end_date::date - _start_date::date + 1)::float)*100, 0)::float) 
 			FROM resource.identifier r);
