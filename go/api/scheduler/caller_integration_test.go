72db4d2dd699ce8d848ae3aa3f65596fc0f41300
Jonathan Enslin <u19103345@tuks.co.za>
2022-09-28 16:59:19 +0200

    Revert "💡(tesiting) Commented out useless code to force checks"
    This reverts commit 5ec5a11236a645301620b15aa34ed36ea3adb5db.

--- go/api/scheduler/caller_integration_test.go
+++ go/api/scheduler/caller_integration_test.go
@@ -9,12 +9,12 @@ import (
 )
 
 func Test_Integration_call(t *testing.T) {
-	// t.Setenv("DATABASE_DSN", "host=127.0.0.1 port=5432 user=admin dbname=arche sslmode=disable")
-	// t.Setenv("DATABASE_MAX_IDLE_CONNECTIONS", "5")
-	// t.Setenv("DATABASE_MAX_OPEN_CONNECTIONS", "25")
-	// t.Setenv("DATABASE_URL", "postgresql://admin:admin@localhost:5432/db?schema=public")
-	// err := db.RegisterAccess()
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	t.Setenv("DATABASE_DSN", "host=127.0.0.1 port=5432 user=admin dbname=arche sslmode=disable")
+	t.Setenv("DATABASE_MAX_IDLE_CONNECTIONS", "5")
+	t.Setenv("DATABASE_MAX_OPEN_CONNECTIONS", "25")
+	t.Setenv("DATABASE_URL", "postgresql://admin:admin@localhost:5432/db?schema=public")
+	err := db.RegisterAccess()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
