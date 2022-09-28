946e54299bdcead8925664585177649bf98eab49
Jonathan Enslin <u19103345@tuks.co.za>
2022-09-28 18:57:48 +0200

    ✨ (scheduler) Work on crossover

--- go/scheduler/ga/validate_test.go
+++ go/scheduler/ga/validate_test.go
@@ -2,6 +2,8 @@ package ga
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIndividual_CheckIfValidDaily(t *testing.T) {
@@ -47,6 +49,69 @@ func TestIndividual_CheckIfValidDaily(t *testing.T) {
 			if got := tt.individual.CheckIfValidDaily(); got != tt.want {
 				t.Errorf("Individual.CheckIfValidDaily() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestIndividual_ValidateIndividual(t *testing.T) {
+	type args struct {
+		domain *Domain
+	}
+	tests := []struct {
+		name       string
+		individual *Individual
+		args       args
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				domain: &Domain{
+					Terminals: []string{"D", "F"},
+				},
+			},
+			individual: &Individual{
+				Gene: [][]string{
+					{
+						"A", "B", "C",
+					},
+				},
+			},
+		},
+		{
+			name: "Test 1",
+			args: args{
+				domain: &Domain{
+					Terminals: []string{"D", "F"},
+				},
+			},
+			individual: &Individual{
+				Gene: [][]string{
+					{
+						"A", "B", "B", "C", "C",
+					},
+				},
+			},
+		},
+		{
+			name: "Test 3",
+			args: args{
+				domain: &Domain{
+					Terminals: []string{"D", "F"},
+				},
+			},
+			individual: &Individual{
+				Gene: [][]string{
+					{
+						"A", "A", "A",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.individual.ValidateIndividual(tt.args.domain)
+			assert.Truef(t, tt.individual.CheckIfValidDaily(), "Expected individual to be valid after call, but got %v", tt.individual)
 		})
 	}
 }
